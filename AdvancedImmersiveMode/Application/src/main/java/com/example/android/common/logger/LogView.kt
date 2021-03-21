a06cb7fba36bc1fb2b3954707d6fdeebd7509cf1
markgray <mark.t.gray@gmail.com>
2021-03-21 06:05:48 -0400

    Added comments to LogNode.kt in AdvancedImmersiveMode

--- AdvancedImmersiveMode/Application/src/main/java/com/example/android/common/logger/LogView.kt
+++ AdvancedImmersiveMode/Application/src/main/java/com/example/android/common/logger/LogView.kt
@@ -22,15 +22,58 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 
-/** Simple TextView which is used to output log data received through the LogNode interface.
+/**
+ * Simple TextView which is used to output log data received through the LogNode interface.
  */
 class LogView : androidx.appcompat.widget.AppCompatTextView, LogNode {
+    /**
+     * @param context The [Context] the view is running in, through which it can access the current
+     * theme, resources, etc.
+     */
     constructor(context: Context) : super(context)
+
+    /**
+     * @param context The [Context] the view is running in, through which it can access the current
+     * theme, resources, etc.
+     * @param attrs The attributes of the XML tag that is inflating the view.
+     */
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    /**
+     * @param context The [Context] the view is running in, through which it can access the current
+     * theme, resources, etc.
+     * @param attrs The attributes of the XML tag that is inflating the view.
+     * @param defStyle A resource identifier of a style resource that supplies default values for
+     * the view. Can be 0 to not look for defaults.
+     */
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle)
 
     /**
-     * Formats the log data and prints it out to the LogView.
+     * Formats the log data and prints it out to the [LogView]. First we initialize our [String]
+     * variable `var priorityStr` to `null`. Then we branch on our [Int] parameter [priority]:
+     *  - [Log.VERBOSE] we set `priorityStr` to "VERBOSE"
+     *  - [Log.DEBUG] we set `priorityStr` to "DEBUG"
+     *  - [Log.INFO] we set `priorityStr` to "INFO"
+     *  - [Log.WARN] we set `priorityStr` to "WARN"
+     *  - [Log.ERROR] we set `priorityStr` to "ERROR"
+     *  - [Log.ASSERT] we set `priorityStr` to "ASSERT"
+     *  - for all other values we do nothing.
+     *
+     * Next we initialize our [String] variable `var exceptionStr` to `null` and if our [Throwable]
+     * parameter [tr] is not `null` we set `exceptionStr` to the loggable stack trace that the
+     * [Log.getStackTraceString] method returns for [tr].
+     *
+     * We next initialize our [StringBuilder] variable `val outputBuilder` to a new instance, and
+     * initialize our [String] variable `val delimiter` to a tab character. We then call our method
+     * [appendIfNotNull] to have it append to `outputBuilder` our `priorityStr`, [tag], [msg], and
+     * `exceptionStr` when they are not `null`, each separated by `delimiter`. Then we have the
+     * [Context] of our `View` run a call to our method [appendToLog] with the [String] created from
+     * `outputBuilder` on the UI thread to have it append that [String] to the bottom of this
+     * [LogView].
+     *
+     * Finally, if our [LogNode] field [next] (the next [LogNode] in the chain) is not `null` we
+     * call its implementation of [LogNode.println] with our parameters.
+     *
      * @param priority Log level of the data being logged.  Verbose, Error, etc.
      * @param tag Tag for for the log data.  Can be used to organize log statements.
      * @param msg The actual message to be logged. The actual message to be logged.
@@ -78,6 +121,7 @@ class LogView : androidx.appcompat.widget.AppCompatTextView, LogNode {
     /** Takes a string and adds to it, with a separator, if the bit to be added isn't null. Since
      * the logger takes so many arguments that might be null, this method helps cut out some of the
      * agonizing tedium of writing the same 3 lines over and over.
+     *
      * @param source StringBuilder containing the text to append to.
      * @param addStr The String to append
      * @param delimiter The String to separate the source and appended strings. A tab or comma,
@@ -101,9 +145,6 @@ class LogView : androidx.appcompat.widget.AppCompatTextView, LogNode {
 
     /** Outputs the string as a new line of log data in the LogView.  */
     fun appendToLog(s: String) {
-        append("""
-    
-    $s
-    """.trimIndent())
+        append("\n$s")
     }
 }
