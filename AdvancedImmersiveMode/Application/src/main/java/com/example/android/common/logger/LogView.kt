376d994c31d01a87e325bc7594dfbb9818a56ba4
markgray <mark.t.gray@gmail.com>
2021-03-21 06:32:16 -0400

    Added comments to LogView.kt in AdvancedImmersiveMode

--- AdvancedImmersiveMode/Application/src/main/java/com/example/android/common/logger/LogView.kt
+++ AdvancedImmersiveMode/Application/src/main/java/com/example/android/common/logger/LogView.kt
@@ -118,15 +118,17 @@ class LogView : androidx.appcompat.widget.AppCompatTextView, LogNode {
         }
     }
 
-    /** Takes a string and adds to it, with a separator, if the bit to be added isn't null. Since
-     * the logger takes so many arguments that might be null, this method helps cut out some of the
+    /**
+     * Takes the [StringBuilder] parameter [source] and adds the [String] parameter [addStr] to it,
+     * with the separator in [String] parameter [delimiter], if [addStr] isn't `null`. Since the
+     * logger takes so many arguments that might be `null`, this method helps cut out some of the
      * agonizing tedium of writing the same 3 lines over and over.
      *
-     * @param source StringBuilder containing the text to append to.
-     * @param addStr The String to append
-     * @param delimiter The String to separate the source and appended strings. A tab or comma,
+     * @param source [StringBuilder] containing the text to append to.
+     * @param addStr The [String] to append
+     * @param delimiter The [String] to separate the source and appended strings. A tab or comma,
      * for instance.
-     * @return The fully concatenated String as a StringBuilder
+     * @return The fully concatenated [String] as a [StringBuilder]
      */
     @Suppress("SameParameterValue")
     private fun appendIfNotNull(source: StringBuilder, addStr: String?, delimiter: String): StringBuilder {
@@ -140,10 +142,16 @@ class LogView : androidx.appcompat.widget.AppCompatTextView, LogNode {
         return source
     }
 
-    // The next LogNode in the chain.
+    /**
+     * The next [LogNode] in the chain.
+     */
     var next: LogNode? = null
 
-    /** Outputs the string as a new line of log data in the LogView.  */
+    /**
+     * Appends the [String] parameter [s] as a new line of log data in the [LogView].
+     *
+     * @param s the [String] to append to the [LogView].
+     */
     fun appendToLog(s: String) {
         append("\n$s")
     }
