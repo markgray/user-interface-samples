6045696f546f926b0d381d3cf902474e3e578689
markgray <mark.t.gray@gmail.com>
2021-03-23 07:14:48 -0400

    Converted Main to kotlin in AppShortcuts

--- AppShortcuts/app/src/main/java/com/example/android/appshortcuts/Main.kt
+++ AppShortcuts/app/src/main/java/com/example/android/appshortcuts/Main.kt
@@ -13,237 +13,195 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.appshortcuts;
+package com.example.android.appshortcuts
 
-import android.app.ListActivity;
-import android.content.Context;
-import android.content.pm.ShortcutInfo;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import androidx.appcompat.app.AlertDialog;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.app.ListActivity
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.ShortcutInfo
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import java.util.ArrayList
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main extends ListActivity implements OnClickListener {
-    static final String TAG = "ShortcutSample";
-
-    private static final String ID_ADD_WEBSITE = "add_website";
-
-    private static final String ACTION_ADD_WEBSITE =
-            "com.example.android.shortcutsample.ADD_WEBSITE";
-
-    private MyAdapter mAdapter;
-
-    private ShortcutHelper mHelper;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.main);
-
-        mHelper = new ShortcutHelper(this);
-
-        mHelper.maybeRestoreAllDynamicShortcuts();
-
-        mHelper.refreshShortcuts(/*force=*/ false);
-
-        if (ACTION_ADD_WEBSITE.equals(getIntent().getAction())) {
+class Main : ListActivity(), View.OnClickListener {
+    private var mAdapter: MyAdapter? = null
+    private var mHelper: ShortcutHelper? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+        mHelper = ShortcutHelper(this)
+        mHelper!!.maybeRestoreAllDynamicShortcuts()
+        mHelper!!.refreshShortcuts( /*force=*/false)
+        if (ACTION_ADD_WEBSITE == intent.action) {
             // Invoked via the manifest shortcut.
-            addWebSite();
+            addWebSite()
         }
-
-        mAdapter = new MyAdapter(this.getApplicationContext());
-        setListAdapter(mAdapter);
+        mAdapter = MyAdapter(this.applicationContext)
+        listAdapter = mAdapter
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        refreshList();
+    override fun onResume() {
+        super.onResume()
+        refreshList()
     }
 
     /**
      * Handle the add button.
      */
-    public void onAddPressed(View v) {
-        addWebSite();
+    @Suppress("UNUSED_PARAMETER")
+    fun onAddPressed(v: View?) {
+        addWebSite()
     }
 
-    private void addWebSite() {
-        Log.i(TAG, "addWebSite");
+    private fun addWebSite() {
+        Log.i(TAG, "addWebSite")
 
         // This is important.  This allows the launcher to build a prediction model.
-        mHelper.reportShortcutUsed(ID_ADD_WEBSITE);
-
-        final EditText editUri = new EditText(this);
-
-        editUri.setHint("http://www.android.com/");
-        editUri.setInputType(EditorInfo.TYPE_TEXT_VARIATION_URI);
-
-        new AlertDialog.Builder(this)
-                .setTitle("Add new website")
-                .setMessage("Type URL of a website")
-                .setView(editUri)
-                .setPositiveButton("Add", (dialog, whichButton) -> {
-                    final String url = editUri.getText().toString().trim();
-                    if (url.length() > 0) {
-                        addUriAsync(url);
-                    }
-                })
-                .show();
-    }
-
-    private void addUriAsync(String uri) {
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                mHelper.addWebSiteShortcut(uri);
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                refreshList();
-            }
-        }.execute();
-    }
-
-    private void refreshList() {
-        mAdapter.setShortcuts(mHelper.getShortcuts());
-    }
-
-    @Override
-    public void onClick(View v) {
-        final ShortcutInfo shortcut = (ShortcutInfo) ((View) v.getParent()).getTag();
-
-        switch (v.getId()) {
-            case R.id.disable:
-                if (shortcut.isEnabled()) {
-                    mHelper.disableShortcut(shortcut);
-                } else {
-                    mHelper.enableShortcut(shortcut);
+        mHelper!!.reportShortcutUsed(ID_ADD_WEBSITE)
+        val editUri = EditText(this)
+        editUri.hint = "http://www.android.com/"
+        editUri.inputType = EditorInfo.TYPE_TEXT_VARIATION_URI
+        AlertDialog.Builder(this)
+            .setTitle("Add new website")
+            .setMessage("Type URL of a website")
+            .setView(editUri)
+            .setPositiveButton("Add") { dialog: DialogInterface?, whichButton: Int ->
+                val url = editUri.text.toString().trim { it <= ' ' }
+                if (url.length > 0) {
+                    addUriAsync(url)
                 }
-                refreshList();
-                break;
-            case R.id.remove:
-                mHelper.removeShortcut(shortcut);
-                refreshList();
-                break;
-        }
+            }
+            .show()
     }
 
-    private static final List<ShortcutInfo> EMPTY_LIST = new ArrayList<>();
-
-    private String getType(ShortcutInfo shortcut) {
-        final StringBuilder sb = new StringBuilder();
-        String sep = "";
-        if (shortcut.isDynamic()) {
-            sb.append(sep);
-            sb.append("Dynamic");
-            sep = ", ";
-        }
-        if (shortcut.isPinned()) {
-            sb.append(sep);
-            sb.append("Pinned");
-            sep = ", ";
-        }
-        if (!shortcut.isEnabled()) {
-            sb.append(sep);
-            sb.append("Disabled");
-            sep = ", ";
-        }
-        return sb.toString();
-    }
-
-    private class MyAdapter extends BaseAdapter {
-        private final Context mContext;
-        private final LayoutInflater mInflater;
-        private List<ShortcutInfo> mList = EMPTY_LIST;
-
-        public MyAdapter(Context context) {
-            mContext = context;
-            mInflater = mContext.getSystemService(LayoutInflater.class);
-        }
-
-        @Override
-        public int getCount() {
-            return mList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return mList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-
-        @Override
-        public boolean areAllItemsEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled(int position) {
-            return true;
-        }
-
-        public void setShortcuts(List<ShortcutInfo> list) {
-            mList = list;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final View view;
-            if (convertView != null) {
-                view = convertView;
-            } else {
-                view = mInflater.inflate(R.layout.list_item, null);
+    private fun addUriAsync(uri: String) {
+        object : AsyncTask<Void?, Void?, Void?>() {
+            protected override fun doInBackground(vararg params: Void?): Void? {
+                mHelper!!.addWebSiteShortcut(uri)
+                return null
             }
 
-            bindView(view, position, mList.get(position));
+            override fun onPostExecute(aVoid: Void?) {
+                refreshList()
+            }
+        }.execute()
+    }
 
-            return view;
+    private fun refreshList() {
+        mAdapter!!.setShortcuts(mHelper!!.shortcuts)
+    }
+
+    override fun onClick(v: View) {
+        val shortcut = (v.parent as View).tag as ShortcutInfo
+        when (v.id) {
+            R.id.disable -> {
+                if (shortcut.isEnabled) {
+                    mHelper!!.disableShortcut(shortcut)
+                } else {
+                    mHelper!!.enableShortcut(shortcut)
+                }
+                refreshList()
+            }
+            R.id.remove -> {
+                mHelper!!.removeShortcut(shortcut)
+                refreshList()
+            }
+        }
+    }
+
+    private fun getType(shortcut: ShortcutInfo): String {
+        val sb = StringBuilder()
+        var sep = ""
+        if (shortcut.isDynamic) {
+            sb.append(sep)
+            sb.append("Dynamic")
+            sep = ", "
+        }
+        if (shortcut.isPinned) {
+            sb.append(sep)
+            sb.append("Pinned")
+            sep = ", "
+        }
+        if (!shortcut.isEnabled) {
+            sb.append(sep)
+            sb.append("Disabled")
+            sep = ", "
+        }
+        return sb.toString()
+    }
+
+    private inner class MyAdapter(private val mContext: Context) : BaseAdapter() {
+        private val mInflater: LayoutInflater
+        private var mList = EMPTY_LIST
+        override fun getCount(): Int {
+            return mList.size
         }
 
-        public void bindView(View view, int position, ShortcutInfo shortcut) {
-            view.setTag(shortcut);
+        override fun getItem(position: Int): Any {
+            return mList[position]
+        }
 
-            final TextView line1 = (TextView) view.findViewById(R.id.line1);
-            final TextView line2 = (TextView) view.findViewById(R.id.line2);
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
 
-            line1.setText(shortcut.getLongLabel());
+        override fun hasStableIds(): Boolean {
+            return false
+        }
 
-            line2.setText(getType(shortcut));
+        override fun areAllItemsEnabled(): Boolean {
+            return true
+        }
 
-            final Button remove = (Button) view.findViewById(R.id.remove);
-            final Button disable = (Button) view.findViewById(R.id.disable);
+        override fun isEnabled(position: Int): Boolean {
+            return true
+        }
 
+        fun setShortcuts(list: List<ShortcutInfo>) {
+            mList = list
+            notifyDataSetChanged()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view: View
+            view = convertView ?: mInflater.inflate(R.layout.list_item, null)
+            bindView(view, position, mList[position])
+            return view
+        }
+
+        fun bindView(view: View, position: Int, shortcut: ShortcutInfo) {
+            view.tag = shortcut
+            val line1 = view.findViewById<View>(R.id.line1) as TextView
+            val line2 = view.findViewById<View>(R.id.line2) as TextView
+            line1.text = shortcut.longLabel
+            line2.text = getType(shortcut)
+            val remove = view.findViewById<View>(R.id.remove) as Button
+            val disable = view.findViewById<View>(R.id.disable) as Button
             disable.setText(
-                    shortcut.isEnabled() ? R.string.disable_shortcut : R.string.enable_shortcut);
-
-            remove.setOnClickListener(Main.this);
-            disable.setOnClickListener(Main.this);
+                if (shortcut.isEnabled) R.string.disable_shortcut else R.string.enable_shortcut)
+            remove.setOnClickListener(this@Main)
+            disable.setOnClickListener(this@Main)
         }
+
+        init {
+            mInflater = mContext.getSystemService(LayoutInflater::class.java)
+        }
+    }
+
+    companion object {
+        const val TAG = "ShortcutSample"
+        private const val ID_ADD_WEBSITE = "add_website"
+        private const val ACTION_ADD_WEBSITE = "com.example.android.shortcutsample.ADD_WEBSITE"
+        private val EMPTY_LIST: List<ShortcutInfo> = ArrayList()
     }
 }
