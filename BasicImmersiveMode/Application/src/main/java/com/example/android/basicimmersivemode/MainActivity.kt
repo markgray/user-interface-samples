19b291b0f93b89bd1c2b9655eb367edf5c746271
markgray <mark.t.gray@gmail.com>
2021-04-01 06:14:12 -0400

    Added comments to MainActivity in BasicImmersiveMode

--- BasicImmersiveMode/Application/src/main/java/com/example/android/basicimmersivemode/MainActivity.kt
+++ BasicImmersiveMode/Application/src/main/java/com/example/android/basicimmersivemode/MainActivity.kt
@@ -18,6 +18,9 @@ package com.example.android.basicimmersivemode
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.android.common.activities.SampleActivityBase
 import com.example.android.common.logger.Log
 import com.example.android.common.logger.LogFragment
@@ -25,10 +28,36 @@ import com.example.android.common.logger.LogWrapper
 import com.example.android.common.logger.MessageOnlyLogFilter
 
 /**
- * A simple launcher activity containing a summary sample description
- * and a few action bar buttons.
+ * Sample demonstrating the use of immersive mode to hide the system and navigation bars for
+ * full screen applications.
+ *
+ * 'Immersive Mode' is a new UI mode which improves 'hide full screen' and 'hide nav bar'
+ * modes, by letting users swipe the bars in and out.
+ *
+ * This sample demonstrates how to enable and disable immersive mode programmatically.
+ *
+ * Immersive mode was introduced in Android 4.4 (Api Level 19). It is toggled using the
+ * SYSTEM_UI_FLAG_IMMERSIVE system ui flag. When combined with the SYSTEM_UI_FLAG_HIDE_NAVIGATION
+ * and SYSTEM_UI_FLAG_FULLSCREEN  flags, hides the navigation and status bars and lets your app
+ * capture all touch events on the screen.
  */
 class MainActivity : SampleActivityBase() {
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * and then we set our content view to our layout file [R.layout.activity_main].
+     *
+     * If our [Bundle] parameter [savedInstanceState] is `null` we are being started for the first
+     * time so we use the [FragmentManager] for interacting with fragments associated with this
+     * activity to begin a [FragmentTransaction] which we save in our variable `val transaction`,
+     * then we construct a new instance of [BasicImmersiveModeFragment] to initialize our variable
+     * `val fragment`, use `transaction` to `replace` the contents of the container with ID
+     * [FRAGTAG] with fragment and commit our [FragmentTransaction] variable `transaction`.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut
+     * down then this is not `null`, and any [Fragment] we added before being shut down will be
+     * restored by the system. If it *is* `null` we are starting for the first time and need to
+     * construct and add our [BasicImmersiveModeFragment] to our UI.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
