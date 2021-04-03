057b955bd783a2d57eded92b3e48ff07383fd2e2
markgray <mark.t.gray@gmail.com>
2021-04-03 07:00:21 -0400

    Converted SampleActivityBase to kotlin in ClippingBasic

--- ClippingBasic/Application/src/main/java/com/example/android/common/activities/SampleActivityBase.kt
+++ ClippingBasic/Application/src/main/java/com/example/android/common/activities/SampleActivityBase.kt
@@ -13,40 +13,51 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+package com.example.android.common.activities
 
-package com.example.android.common.activities;
-
-import android.os.Bundle;
-import androidx.fragment.app.FragmentActivity;
-
-import com.example.android.common.logger.Log;
-import com.example.android.common.logger.LogWrapper;
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.example.android.common.logger.Log
+import com.example.android.common.logger.LogWrapper
 
 /**
  * Base launcher activity, to handle most of the common plumbing for samples.
  */
-public class SampleActivityBase extends FragmentActivity {
-
-    public static final String TAG = "SampleActivityBase";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+open class SampleActivityBase : FragmentActivity() {
+    /**
+     * Called when the activity is starting, we just call our super's implementation of `onCreate`.
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
+     */
+    @Suppress("RedundantOverride")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    @Override
-    protected  void onStart() {
-        super.onStart();
-        initializeLogging();
+    /**
+     * Called after [onCreate] or after [onRestart] when the activity had been stopped, but is now
+     * again being displayed to the user. It will usually be followed by [onResume]. This is a good
+     * place to begin drawing visual elements, running animations, etc. We call our super's `onStart`,
+     * then call our method [initializeLogging] to up targets to receive log data.
+     */
+    override fun onStart() {
+        super.onStart()
+        initializeLogging()
     }
 
-    /** Set up targets to receive log data */
-    public void initializeLogging() {
+    /** Set up targets to receive log data  */
+    open fun initializeLogging() {
         // Using Log, front-end to the logging chain, emulates android.util.log method signatures.
         // Wraps Android's native log framework
-        LogWrapper logWrapper = new LogWrapper();
-        Log.setLogNode(logWrapper);
+        val logWrapper = LogWrapper()
+        Log.setLogNode(logWrapper)
+        Log.i(TAG, "Ready")
+    }
 
-        Log.i(TAG, "Ready");
+    companion object {
+        /**
+         * TAG to be used for logging.
+         */
+        const val TAG = "SampleActivityBase"
     }
 }
