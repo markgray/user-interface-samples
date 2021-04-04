792216d979cd284b53200479c0e781068e4547e9
markgray <mark.t.gray@gmail.com>
2021-04-04 07:27:23 -0400

    Converted ClippingBasicFragment to kotlin in ClippingBasic

--- ClippingBasic/Application/src/main/java/com/example/android/clippingbasic/ClippingBasicFragment.kt
+++ ClippingBasic/Application/src/main/java/com/example/android/clippingbasic/ClippingBasicFragment.kt
@@ -13,127 +13,113 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.clippingbasic;
+package com.example.android.clippingbasic
 
-import com.example.android.common.logger.Log;
-
-import android.graphics.Outline;
-import androidx.fragment.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewOutlineProvider;
-import android.widget.Button;
-import android.widget.TextView;
+import android.graphics.Outline
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewOutlineProvider
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.android.common.logger.Log
 
 /**
- * This sample shows how to clip a {@link View} using an {@link Outline}.
+ * This sample shows how to clip a [View] using an [Outline].
  */
-public class ClippingBasicFragment extends Fragment {
+class ClippingBasicFragment : Fragment() {
+    /**
+     * Store the click count so that we can show a different text on every click.
+     */
+    private var mClickCount = 0
 
-    private final static String TAG = "ClippingBasicFragment";
+    /**
+     * The [Outline] used to clip the image with.
+     */
+    private lateinit var mOutlineProvider: ViewOutlineProvider
 
-    /* Store the click count so that we can show a different text on every click. */
-    private int mClickCount = 0;
+    /**
+     * An array of texts.
+     */
+    private lateinit var mSampleTexts: Array<String>
 
-    /* The {@Link Outline} used to clip the image with. */
-    private ViewOutlineProvider mOutlineProvider;
-
-    /* An array of texts. */
-    private String[] mSampleTexts;
-
-    /* A reference to a {@Link TextView} that shows different text strings when clicked. */
-    private TextView mTextView;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        mOutlineProvider = new ClipOutlineProvider();
-        mSampleTexts = getResources().getStringArray(R.array.sample_texts);
+    /**
+     * A reference to a [TextView] that shows different text strings when clicked.
+     */
+    private lateinit var mTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        mOutlineProvider = ClipOutlineProvider()
+        mSampleTexts = resources.getStringArray(R.array.sample_texts)
     }
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.clipping_basic_fragment, container, false);
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.clipping_basic_fragment, container, false)
     }
 
-    @Override
-    public void onViewCreated(final View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         /* Set the initial text for the TextView. */
-        mTextView = (TextView) view.findViewById(R.id.text_view);
-        changeText();
+        mTextView = view.findViewById<View>(R.id.text_view) as TextView
+        changeText()
+        val clippedView = view.findViewById<View>(R.id.frame)
 
-
-        final View clippedView = view.findViewById(R.id.frame);
-
-        /* Sets the OutlineProvider for the View. */
-        clippedView.setOutlineProvider(mOutlineProvider);
+        /* Sets the OutlineProvider for the View. */clippedView.outlineProvider = mOutlineProvider
 
         /* When the button is clicked, the text is clipped or un-clipped. */
-        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View bt) {
-                // Toggle whether the View is clipped to the outline
-                if (clippedView.getClipToOutline()) {
-                    /* The Outline is set for the View, but disable clipping. */
-                    clippedView.setClipToOutline(false);
-
-                    Log.d(TAG, String.format("Clipping to outline is disabled"));
-                    ((Button) bt).setText(R.string.clip_button);
-                } else {
-                    /* Enables clipping on the View. */
-                    clippedView.setClipToOutline(true);
-
-                    Log.d(TAG, String.format("Clipping to outline is enabled"));
-                    ((Button) bt).setText(R.string.unclip_button);
-                }
+        view.findViewById<View>(R.id.button).setOnClickListener { bt ->
+            // Toggle whether the View is clipped to the outline
+            if (clippedView.clipToOutline) {
+                /* The Outline is set for the View, but disable clipping. */
+                clippedView.clipToOutline = false
+                Log.d(TAG, String.format("Clipping to outline is disabled"))
+                (bt as Button).setText(R.string.clip_button)
+            } else {
+                /* Enables clipping on the View. */
+                clippedView.clipToOutline = true
+                Log.d(TAG, String.format("Clipping to outline is enabled"))
+                (bt as Button).setText(R.string.unclip_button)
             }
-        });
+        }
 
         /* When the text is clicked, a new string is shown. */
-        view.findViewById(R.id.text_view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mClickCount++;
-
-                // Update the text in the TextView
-                changeText();
-
-                // Invalidate the outline just in case the TextView changed size
-                clippedView.invalidateOutline();
-            }
-        });
+        view.findViewById<View>(R.id.text_view).setOnClickListener {
+            mClickCount++
+            // Update the text in the TextView
+            changeText()
+            // Invalidate the outline just in case the TextView changed size
+            clippedView.invalidateOutline()
+        }
     }
 
-    private void changeText() {
+    private fun changeText() {
         // Compute the position of the string in the array using the number of strings
         //  and the number of clicks.
-        String newText = mSampleTexts[mClickCount % mSampleTexts.length];
+        val newText = mSampleTexts[mClickCount % mSampleTexts.size]
 
         /* Once the text is selected, change the TextView */
-        mTextView.setText(newText);
-        Log.d(TAG, String.format("Text was changed."));
-
-
+        mTextView.text = newText
+        Log.d(TAG, String.format("Text was changed."))
     }
 
     /**
-     * A {@link ViewOutlineProvider} which clips the view with a rounded rectangle which is inset
+     * A [ViewOutlineProvider] which clips the view with a rounded rectangle which is inset
      * by 10%
      */
-    private class ClipOutlineProvider extends ViewOutlineProvider {
-
-        @Override
-        public void getOutline(View view, Outline outline) {
-            final int margin = Math.min(view.getWidth(), view.getHeight()) / 10;
-            outline.setRoundRect(margin, margin, view.getWidth() - margin,
-                    view.getHeight() - margin, margin / 2);
+    private inner class ClipOutlineProvider : ViewOutlineProvider() {
+        override fun getOutline(view: View, outline: Outline) {
+            val margin = view.width.coerceAtMost(view.height) / 10
+            outline.setRoundRect(margin, margin, view.width - margin,
+                view.height - margin, (margin / 2).toFloat())
         }
+    }
 
+    companion object {
+        private const val TAG = "ClippingBasicFragment"
     }
 }
