6661b5e45a066d5b15f6dbf0b3788f7a1e15f803
markgray <mark.t.gray@gmail.com>
2021-04-06 07:12:51 -0400

    Converted DarkThemeApplication to kotlin in DarkTheme

--- DarkTheme/Application/src/main/java/com/example/android/darktheme/DarkThemeApplication.kt
+++ DarkTheme/Application/src/main/java/com/example/android/darktheme/DarkThemeApplication.kt
@@ -13,21 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.darktheme
 
-package com.example.android.darktheme;
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
 
-import android.app.Application;
-import android.content.SharedPreferences;
-
-import androidx.preference.PreferenceManager;
-
-public class DarkThemeApplication extends Application {
-
-    public void onCreate() {
-        super.onCreate();
-        SharedPreferences sharedPreferences =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        String themePref = sharedPreferences.getString("themePref", ThemeHelper.DEFAULT_MODE);
-        ThemeHelper.applyTheme(themePref);
+/**
+ * Base class for maintaining global application state. Here we provide our own implementation by
+ * creating a subclass and specifying the fully-qualified name of this subclass as the "android:name"
+ * attribute in our AndroidManifest.xml's <application> tag.
+ *
+ * The [Application] class, or your subclass of the [Application] class, is instantiated before any
+ * other class when the process for your application/package is created
+ *
+ * It is used as the android:name attribute of the application element in our AndroidManifest.xml
+ */
+@Suppress("unused")
+class DarkThemeApplication : Application() {
+    /**
+     * Called when the application is starting, before any activity, service, or receiver objects
+     * (excluding content providers) have been created. First we call our super's implementation of
+     * `onCreate`, then we initialize our [SharedPreferences] variable `val sharedPreferences` to an
+     * instance that points to the default file that is used by the preference framework for our
+     * context, and we initialize our [String] variable `val themePref` to the [String] value from
+     * `sharedPreferences` that is stored in it under the key "themePref" defaulting to the [String]
+     * [ThemeHelper.DEFAULT_MODE] ("default"). Finally we call the [ThemeHelper.applyTheme] method
+     * with `themePref` to have it set the default night mode to `themePref`.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val themePref = sharedPreferences.getString("themePref", ThemeHelper.DEFAULT_MODE)
+        ThemeHelper.applyTheme(themePref!!)
     }
 }
