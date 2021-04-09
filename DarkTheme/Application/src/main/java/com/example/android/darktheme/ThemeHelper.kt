50f4e85bdfaa1918a93346ed92e2532bf609a37c
markgray <mark.t.gray@gmail.com>
2021-04-09 06:16:17 -0400

    Converted ThemeHelper to kotlin in DarkTheme

--- DarkTheme/Application/src/main/java/com/example/android/darktheme/ThemeHelper.kt
+++ DarkTheme/Application/src/main/java/com/example/android/darktheme/ThemeHelper.kt
@@ -13,38 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.darktheme
 
-package com.example.android.darktheme;
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
 
-import android.os.Build;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.os.BuildCompat;
-
-public class ThemeHelper {
-
-    public static final String LIGHT_MODE = "light";
-    public static final String DARK_MODE = "dark";
-    public static final String DEFAULT_MODE = "default";
-
-    public static void applyTheme(@NonNull String themePref) {
-        switch (themePref) {
-            case LIGHT_MODE: {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                break;
+object ThemeHelper {
+    private const val LIGHT_MODE = "light"
+    private const val DARK_MODE = "dark"
+    const val DEFAULT_MODE = "default"
+    fun applyTheme(themePref: String) {
+        when (themePref) {
+            LIGHT_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-            case DARK_MODE: {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                break;
+            DARK_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             }
-            default: {
+            else -> {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
                 } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
                 }
-                break;
             }
         }
     }
