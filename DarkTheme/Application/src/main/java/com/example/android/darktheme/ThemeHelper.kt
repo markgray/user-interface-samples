17404c246ee1b051da1098e290e8adedd6abbaa0
markgray <mark.t.gray@gmail.com>
2021-04-09 07:19:53 -0400

    Added comments to ThemeHelper in DarkTheme

--- DarkTheme/Application/src/main/java/com/example/android/darktheme/ThemeHelper.kt
+++ DarkTheme/Application/src/main/java/com/example/android/darktheme/ThemeHelper.kt
@@ -18,10 +18,51 @@ package com.example.android.darktheme
 import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 
+/**
+ * This file holds a single method: [applyTheme] which is called to apply whichever theme the user
+ * has chosen, either "light", "dark" or "default" using the [AppCompatDelegate.setDefaultNightMode]
+ * method. It is called from the `onCreate` override of [DarkThemeApplication] to set it to the theme
+ * stored under the key "themePref" in the app's shared preferences (defaulting to "default" is the
+ * preference has not been set) and by the `onCreatePreferences` override of [SettingsFragment] when
+ * the user sets a new value for the "themePref" shared preference.
+ */
 object ThemeHelper {
+    /**
+     * Value of `themePref` to select "light" mode
+     */
     private const val LIGHT_MODE = "light"
+
+    /**
+     * Value of `themePref` to select "dark" mode
+     */
     private const val DARK_MODE = "dark"
+
+    /**
+     * Value of `themePref` to select "default" mode
+     */
     const val DEFAULT_MODE = "default"
+
+    /**
+     * Sets the default night mode for our app based on the value of its [String] parameter [themePref].
+     * We branch on the value of [themePref]:
+     *  - [LIGHT_MODE] we call [AppCompatDelegate.setDefaultNightMode] with [AppCompatDelegate.MODE_NIGHT_NO]
+     *  to have it set the mode to one which always uses a light mode, enabling `notnight` qualified
+     *  resources regardless of the time.
+     *  - [DARK_MODE] we call [AppCompatDelegate.setDefaultNightMode] with [AppCompatDelegate.MODE_NIGHT_YES]
+     *  to have it set the mode to one which always uses a dark mode, enabling night qualified resources
+     *  regardless of the time.
+     *  - For all other values of [themePref] we branch based on the version of android we are running:
+     *      - Greater than or equal to "Q": we call [AppCompatDelegate.setDefaultNightMode] with
+     *      [AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM] to have it set the mode to one which uses
+     *      the system's night mode setting to determine if it is night or not.
+     *      - Less than "Q": we call [AppCompatDelegate.setDefaultNightMode] with
+     *      [AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY] to have it set the mode to one which uses a
+     *      dark mode when the system's 'Battery Saver' feature is enabled, otherwise it uses a
+     *      'light mode'.
+     *
+     * @param themePref a [String] selecting which default night mode the user has selected, one of
+     * "light", "dark" or "default".
+     */
     fun applyTheme(themePref: String) {
         when (themePref) {
             LIGHT_MODE -> {
