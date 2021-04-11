411b36f30a5028ff9b2a7fcf00bebb778c39d40a
markgray <mark.t.gray@gmail.com>
2021-04-11 06:24:49 -0400

    Added comments to NewDocumentActivity in DocumentCentricApps

--- DocumentCentricApps/Application/src/main/java/com/example/android/documentcentricapps/NewDocumentActivity.kt
+++ DocumentCentricApps/Application/src/main/java/com/example/android/documentcentricapps/NewDocumentActivity.kt
@@ -25,7 +25,6 @@ import androidx.appcompat.app.AppCompatActivity
  * Represents a "document" in the new overview notion. This is just a placeholder.
  * Real world examples of this could be:
  *
- *
  *  * Document Editing
  *  * Browser tabs
  *  * Message composition
@@ -34,23 +33,51 @@ import androidx.appcompat.app.AppCompatActivity
  *
  */
 class NewDocumentActivity : AppCompatActivity() {
+    /**
+     * The [TextView] in our UI with ID [R.id.hello_new_document_text_view] which is used to display
+     * our [Int] field [mDocumentCount] which is passed to use as an extra in the [Intent] which
+     * [DocumentCentricActivity] uses to launch us.
+     */
     private lateinit var mDocumentCounterTextView: TextView
+
+    /**
+     * The [Int] passed us as an extra in the [Intent] which [DocumentCentricActivity] uses to
+     * launch us. We just display it in our [TextView] field [mDocumentCounterTextView].
+     */
     private var mDocumentCount = 0
+
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`
+     * then we set our content view to our layout file [R.layout.activity_new_document] (it consists
+     * of a root `LinearLayout` holding our [TextView] field [mDocumentCounterTextView] and a
+     * `Button` labeled "Remove from Overview" whose android:onClick attribute calls our method
+     * [onRemoveFromOverview] to have it call [finishAndRemoveTask] to close our activity). We then
+     * initialize our [Int] field [mDocumentCount] to the value stored as an extra in the [Intent]
+     * that started this activity under the key [DocumentCentricActivity.KEY_EXTRA_NEW_DOCUMENT_COUNTER],
+     * and initialize our [TextView] field [mDocumentCounterTextView] by finding the [View] in our
+     * UI with the ID [R.id.hello_new_document_text_view]. Finally we call our [setDocumentCounterText]
+     * with the resource ID [R.string.hello_new_document_counter] (points to the [String] "Hello
+     * Document %s!") to have it use that [String] to format [mDocumentCount] and display the result
+     * as the text of [mDocumentCounterTextView].
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut
+     * down then this [Bundle] contains the data it most recently supplied in [onSaveInstanceState].
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_document)
         mDocumentCount = intent
             .getIntExtra(DocumentCentricActivity.KEY_EXTRA_NEW_DOCUMENT_COUNTER, 0)
-        mDocumentCounterTextView = findViewById(
-            R.id.hello_new_document_text_view)
+        mDocumentCounterTextView = findViewById(R.id.hello_new_document_text_view)
         setDocumentCounterText(R.string.hello_new_document_counter)
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        /* If {@link Intent#FLAG_ACTIVITY_MULTIPLE_TASK} has not been used this Activity
-        will be reused.
-         */setDocumentCounterText(R.string.reusing_document_counter)
+        /**
+         * If `Intent.FLAG_ACTIVITY_MULTIPLE_TASK` has not been used this Activity will be reused.
+         */
+        setDocumentCounterText(R.string.reusing_document_counter)
     }
 
     @Suppress("UNUSED_PARAMETER")
