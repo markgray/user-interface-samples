aeec33357bc821e6f2c5dbec20bed6089d3c1c4e
markgray <mark.t.gray@gmail.com>
2021-04-18 06:21:53 -0400

    Added comments to QueryBuilder.kt in DownloadableFontsKotlin

--- DownloadableFontsKotlin/app/src/main/java/com/example/android/downloadablefonts/QueryBuilder.kt
+++ DownloadableFontsKotlin/app/src/main/java/com/example/android/downloadablefonts/QueryBuilder.kt
@@ -16,15 +16,54 @@
 
 package com.example.android.downloadablefonts
 
+import androidx.core.provider.FontRequest
+
 /**
  * Builder class for constructing a query for downloading a font.
+ *
+ * @param familyName the family name of the font that is to be requested.
+ * @param width The width of the font, more than [Constants.WIDTH_MIN] (0) and less than
+ * [Constants.WIDTH_MAX] (1000), with the default [Constants.WIDTH_DEFAULT] (100). If non-`null`
+ * its value is appended to the "&width=" query string of the query that is built.
+ * @param weight The weight of the font, greater than [Constants.WEIGHT_MIN] (0) and less than
+ * [Constants.WEIGHT_MAX] (1000) with the default [Constants.WEIGHT_DEFAULT] (400). If non-`null`
+ * its value is appended to the "&weight=" query string of the query that is built.
+ * @param italic The italic value of the font, greater than or equal to [Constants.ITALIC_MIN] (0f)
+ * and less than or equal to [Constants.ITALIC_MAX] (1.0f) with the default [Constants.ITALIC_DEFAULT]
+ * (0f). If non-`null` its value is appended to the "&italic=" query string of the query that is built.
+ * @param besteffort `true` or `false` value of the "&besteffort=" query string added to our query.
+ * It defaults to `true` and if `true` and your query specifies a valid family name but the requested
+ * width/weight/italic value is not supported the font provider will return the best match it can
+ * find within the family.
  */
-internal class QueryBuilder(private val familyName: String,
-                            val width: Float? = null,
-                            val weight: Int? = null,
-                            val italic: Float? = null,
-                            val besteffort: Boolean? = null) {
+internal class QueryBuilder(
+    private val familyName: String,
+    val width: Float? = null,
+    val weight: Int? = null,
+    val italic: Float? = null,
+    val besteffort: Boolean? = null
+) {
 
+    /**
+     * Builds `this` [QueryBuilder] into a query that can be used in a [FontRequest] for a font from
+     * a font provider. If our fields [weight], [width], [italic], and [besteffort] are all `null`
+     * we return our font family name field [familyName] to the caller. Otherwise we initialize our
+     * [StringBuilder] variable `val builder` to a new instance and add the [String] "name=" followed
+     * by [familyName] and proceed to check each of our fields for a non-`null` value and add query
+     * strings for the non-`null` fields to `builder` as follows:
+     *  - [weight] non-`null` adds the query string "&weight=" followed by the string value of the
+     *  [Int] field [weight] to `builder`
+     *  - [width] non-`null` adds the query string "&width=" followed by the string value of the
+     *  [Float] field [width] to `builder`
+     *  - [italic] non-`null` adds the query string "&italic=" followed by the string value of the
+     *  [Float] field [italic] to `builder`
+     *  - [besteffort] non-`null` adds the query string "&besteffort=" followed by the string value
+     *  of the [Boolean] field [besteffort] ("true" or "false") to `builder`
+     *
+     * Finally we return the [String] value of `builder` to the caller.
+     *
+     * @return the query [String] to be used when constructing a [FontRequest]
+     */
     fun build(): String {
         if (weight == null && width == null && italic == null && besteffort == null) {
             return familyName
