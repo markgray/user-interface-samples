c5a0ffc376a386e0702807b39ab21e7f409383af
markgray <mark.t.gray@gmail.com>
2021-04-26 05:51:03 -0400

    Converted ElevationBasicFragment to kotlin in ElevationBasic

--- ElevationBasic/Application/src/main/java/com/example/android/elevationbasic/ElevationBasicFragment.kt
+++ ElevationBasic/Application/src/main/java/com/example/android/elevationbasic/ElevationBasicFragment.kt
@@ -13,62 +13,57 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.elevationbasic
 
-package com.example.android.elevationbasic;
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android.common.logger.Log
 
-import androidx.fragment.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.android.common.logger.Log;
-
-public class ElevationBasicFragment extends Fragment {
-
-    private final static String TAG = "ElevationBasicFragment";
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+class ElevationBasicFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         /**
          * Inflates an XML containing two shapes: the first has a fixed elevation
          * and the second ones raises when tapped.
          */
-        View rootView = inflater.inflate(R.layout.elevation_basic, container, false);
-
-        View shape2 = rootView.findViewById(R.id.floating_shape_2);
-
+        val rootView = inflater.inflate(R.layout.elevation_basic, container, false)
+        val shape2 = rootView.findViewById<View>(R.id.floating_shape_2)
         /**
          * Sets a {@Link View.OnTouchListener} that responds to a touch event on shape2.
          */
-        shape2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getActionMasked();
-                /* Raise view on ACTION_DOWN and lower it on ACTION_UP. */
-                switch (action) {
-                    case MotionEvent.ACTION_DOWN:
-                        Log.d(TAG, "ACTION_DOWN on view.");
-                        view.setTranslationZ(120);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        Log.d(TAG, "ACTION_UP on view.");
-                        view.setTranslationZ(0);
-                        break;
-                    default:
-                        return false;
+        shape2.setOnTouchListener(OnTouchListener { view, motionEvent ->
+            when (motionEvent.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d(TAG, "ACTION_DOWN on view.")
+                    view.translationZ = 120f
                 }
-                return true;
+                MotionEvent.ACTION_UP -> {
+                    Log.d(TAG, "ACTION_UP on view.")
+                    view.translationZ = 0f
+                }
+                else -> return@OnTouchListener false
             }
-        });
-        return rootView;
+            true
+        })
+        return rootView
+    }
+
+    companion object {
+        /**
+         * TAG used for logging
+         */
+        private const val TAG = "ElevationBasicFragment"
     }
 }
