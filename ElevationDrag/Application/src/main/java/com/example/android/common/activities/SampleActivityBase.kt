15e3313e97b48d15284d7ba0d2f5d9f92244be30
markgray <mark.t.gray@gmail.com>
2021-04-29 05:38:44 -0400

    Converted contents of common/logger/ to kotlin in ElevationDrag

--- ElevationDrag/Application/src/main/java/com/example/android/common/activities/SampleActivityBase.kt
+++ ElevationDrag/Application/src/main/java/com/example/android/common/activities/SampleActivityBase.kt
@@ -50,7 +50,7 @@ open class SampleActivityBase : FragmentActivity() {
         // Using Log, front-end to the logging chain, emulates android.util.log method signatures.
         // Wraps Android's native log framework
         val logWrapper = LogWrapper()
-        Log.setLogNode(logWrapper)
+        Log.logNode = logWrapper
         Log.i(TAG, "Ready")
     }
 
