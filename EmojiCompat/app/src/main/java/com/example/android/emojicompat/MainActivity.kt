2a8d246389ee08ba408642d97183b38af3c632f2
markgray <mark.t.gray@gmail.com>
2021-04-29 07:58:25 -0400

    Added comments to MainActivity in EmojiCompat

--- EmojiCompat/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
+++ EmojiCompat/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.core.provider.FontRequest
 import androidx.emoji.bundled.BundledEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
 import androidx.emoji.text.FontRequestEmojiCompatConfig
+import androidx.emoji.widget.EmojiAppCompatButton
+import androidx.emoji.widget.EmojiAppCompatEditText
+import androidx.emoji.widget.EmojiAppCompatTextView
 import java.lang.ref.WeakReference
 
 /**
@@ -32,7 +35,45 @@ import java.lang.ref.WeakReference
  */
 class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting.
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`.
+     * We then call our method [initEmojiCompat] to have it initialize [EmojiCompat] to use either
+     * the bundled "Noto Color Emoji Compat" font bundled with our app due to the presence of a
+     * android:name="fontProviderRequests" meta-data element in our AndroidManifest file (downloads
+     * the font when the app is installed) or the same font downloaded using a [FontRequest] depending
+     * on whether our [USE_BUNDLED_EMOJI] field is `true` or `false`. Next we set our content view to
+     * our layout file [R.layout.activity_main] which consists of a `ScrollView` root view holding
+     * a vertical `LinearLayout` with an [EmojiAppCompatTextView], an [EmojiAppCompatEditText], an
+     * [EmojiAppCompatButton], a [TextView] and our [CustomTextView] custom [TextView].
+     *
+     * We then proceed to locate each of the above text displaying views in order to set their text
+     * to a [CharSequence] which include emojis from [EmojiCompat]:
+     *  - `val emojiTextView` is initialized by finding the [TextView] in our UI with resource ID
+     *  [R.id.emoji_text_view]. It is an [EmojiAppCompatTextView] so we can just set its `text`
+     *  property to the emoji containing formatted string "Emoji TextView [EMOJI]" created by calling
+     *  the [getString] method with the resource ID [R.string.emoji_text_view] formatting string and
+     *  the [EMOJI] string constant.
+     *  - `val emojiEditText` is initialized by finding the [TextView] in our UI with resource ID
+     *  [R.id.emoji_edit_text]. It is an [EmojiAppCompatEditText] so we can just set its `text`
+     *  property to the emoji containing formatted string "Emoji EditText [EMOJI]" created by calling
+     *  the [getString] method with the resource ID [R.string.emoji_edit_text] formatting string and
+     *  the [EMOJI] string constant.
+     *  - `val emojiButton` is initialized by finding the [TextView] in our UI with resource ID
+     *  [R.id.emoji_button]. It is an [EmojiAppCompatButton] so we can just set its `text` property
+     *  to the emoji containing formatted string "Emoji Button [EMOJI]" created by calling the
+     *  [getString] method with the resource ID [R.string.emoji_button] formatting string and the
+     *  [EMOJI] string constant.
+     *  - `val regularTextView` is initialized by finding the [TextView] in our UI with resource ID
+     *  [R.id.regular_text_view]. This is a regular [TextView] without [EmojiCompat] support so we
+     *  have to manually process the text and to do this we fetch the singleton [EmojiCompat] instance
+     *  and register an instance of our initialization callback [InitCallback] constructed to use the
+     *  singleton [EmojiCompat] to process the text and set the text of `regularTextView` to it in
+     *  its `onInitialized` override (**Note:** if initialization has already occurred `onInitialized`
+     *  is called immediately).
+     *  - `val customTextView` is initialized by finding the [TextView] in our UI with resource ID
+     *  [R.id.emoji_custom_text_view]. It is an instance of our [CustomTextView] so we can just set
+     *  its `text` property  to the emoji containing formatted string "Custom TextView [EMOJI]" created
+     *  by calling the [getString] method with the resource ID [R.string.custom_text_view] formatting
+     *  string and the [EMOJI] string constant.
      *
      * @param savedInstanceState We do not override [onSaveInstanceState] so do not use.
      */
@@ -116,6 +157,6 @@ class MainActivity : AppCompatActivity() {
         // [U+1F469] (WOMAN) + [U+200D] (ZERO WIDTH JOINER) + [U+1F3A4] (MICROPHONE)
         private const val WOMAN_SINGER = "\uD83D\uDC69\u200D\uD83C\uDFA4"
         private const val EMOTIONS = "\uD83D\uDE0D\uD83E\uDD29\n"
-        const val EMOJI = "$WOMAN_TECHNOLOGIST $WOMAN_SINGER $EMOTIONS"
+        const val EMOJI: String = "$WOMAN_TECHNOLOGIST $WOMAN_SINGER $EMOTIONS"
     }
 }
