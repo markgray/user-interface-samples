c26ffcd9743a96750785c41f67b9e3daadb65830
markgray <mark.t.gray@gmail.com>
2021-05-11 07:11:24 -0400

    Converted MainActivity to kotlin in TextStyling

--- TextStyling/app/src/main/java/com/android/example/text/styling/MainActivity.kt
+++ TextStyling/app/src/main/java/com/android/example/text/styling/MainActivity.kt
@@ -13,29 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.example.text.styling;
+package com.android.example.text.styling
 
-import android.graphics.Typeface;
-import android.os.Bundle;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
-
-import com.android.example.text.styling.parser.Parser;
-import com.android.example.text.styling.renderer.MarkdownBuilder;
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.android.example.text.styling.parser.Parser
+import com.android.example.text.styling.renderer.MarkdownBuilder
 
 /**
  * This sample demonstrates techniques for stying text; it is not intended to be a full markdown
  * parser.
  */
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.styled_text);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.styled_text)
 
         // This is a simple markdown parser, where:
         // Paragraphs starting with “> ” are transformed into quotes. Quotes can't contain
@@ -43,13 +39,12 @@ public class MainActivity extends AppCompatActivity {
         // Text enclosed in “`” will be transformed into inline code block
         // Lines starting with “+ ” or “* ” will be transformed into bullet points. Bullet
         // points can contain nested markdown elements, like code.
-        int bulletPointColor = ContextCompat.getColor(this, R.color.colorAccent);
-        int codeBackgroundColor = ContextCompat.getColor(this, R.color.code_background);
-        Typeface codeBlockTypeface = ResourcesCompat.getFont(this, R.font.inconsolata);
-
-        CharSequence text = new MarkdownBuilder(bulletPointColor, codeBackgroundColor,
-                codeBlockTypeface, new Parser())
-                .markdownToSpans(getString(R.string.display_text));
-        textView.setText(text);
+        val bulletPointColor = ContextCompat.getColor(this, R.color.colorAccent)
+        val codeBackgroundColor = ContextCompat.getColor(this, R.color.code_background)
+        val codeBlockTypeface = ResourcesCompat.getFont(this, R.font.inconsolata)
+        val text: CharSequence = MarkdownBuilder(bulletPointColor, codeBackgroundColor,
+            codeBlockTypeface, Parser())
+            .markdownToSpans(getString(R.string.display_text))
+        textView.text = text
     }
 }
