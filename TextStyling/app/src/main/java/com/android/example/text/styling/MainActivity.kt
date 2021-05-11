bc1b493eb30e3fff051eed0f7f4bf7ed28deaf51
markgray <mark.t.gray@gmail.com>
2021-05-11 07:44:44 -0400

    Added comments to MainActivity in TextStyling

--- TextStyling/app/src/main/java/com/android/example/text/styling/MainActivity.kt
+++ TextStyling/app/src/main/java/com/android/example/text/styling/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.android.example.text.styling
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -28,6 +29,21 @@ import com.android.example.text.styling.renderer.MarkdownBuilder
  * parser.
  */
 class MainActivity : AppCompatActivity() {
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * then we set our content view to our layout file [R.layout.activity_main] which consists of a
+     * `ConstraintLayout` root view holding a single [TextView] and we initialize our [TextView]
+     * variable `val textView` by finding that view whose ID is [R.id.styled_text]. We initialize
+     * our [Int] variable `val bulletPointColor` by retrieving the color [R.color.colorAccent] which
+     * is `FF4081` -- a pastel shade of red. We initialize our [Int] variable `val codeBackgroundColor`
+     * by retrieving the color [R.color.code_background] which is `BBBBBB` -- a light gray.
+     *
+     * We then initialize our [Typeface] variable `val codeBlockTypeface` by using the `font-family`
+     * which is defined in the font/inconsolata.xml file to download the "Inconsolata" font from the
+     * font provider "com.google.android.gms.fonts".
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         // points can contain nested markdown elements, like code.
         val bulletPointColor = ContextCompat.getColor(this, R.color.colorAccent)
         val codeBackgroundColor = ContextCompat.getColor(this, R.color.code_background)
-        val codeBlockTypeface = ResourcesCompat.getFont(this, R.font.inconsolata)
+        val codeBlockTypeface: Typeface? = ResourcesCompat.getFont(this, R.font.inconsolata)
         val text: CharSequence = MarkdownBuilder(bulletPointColor, codeBackgroundColor,
             codeBlockTypeface, Parser())
             .markdownToSpans(getString(R.string.display_text))
