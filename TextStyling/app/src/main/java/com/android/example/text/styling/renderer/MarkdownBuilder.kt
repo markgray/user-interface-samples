f077dd96c7f300c1e7b5ad353b3578efa79c1e48
markgray <mark.t.gray@gmail.com>
2021-05-12 07:21:04 -0400

    Converted MarkdownBuilder to kotlin in TextStyling

--- TextStyling/app/src/main/java/com/android/example/text/styling/renderer/MarkdownBuilder.kt
+++ TextStyling/app/src/main/java/com/android/example/text/styling/renderer/MarkdownBuilder.kt
@@ -13,116 +13,102 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.example.text.styling.renderer;
+package com.android.example.text.styling.renderer
 
-import android.graphics.Typeface;
-import androidx.annotation.ColorInt;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.SpannedString;
-import android.text.style.LeadingMarginSpan;
-import android.text.style.RelativeSizeSpan;
-import android.text.style.StyleSpan;
-
-import com.android.example.text.styling.parser.Element;
-import com.android.example.text.styling.parser.Parser;
-import com.android.example.text.styling.parser.TextMarkdown;
-import com.android.example.text.styling.renderer.spans.BulletPointSpan;
-import com.android.example.text.styling.renderer.spans.CodeBlockSpan;
+import android.graphics.Typeface
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.SpannedString
+import android.text.style.LeadingMarginSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
+import androidx.annotation.ColorInt
+import com.android.example.text.styling.parser.Element
+import com.android.example.text.styling.parser.Parser
+import com.android.example.text.styling.renderer.spans.BulletPointSpan
+import com.android.example.text.styling.renderer.spans.CodeBlockSpan
 
 /**
  * Renders the text as simple markdown, using spans.
  */
-public class MarkdownBuilder {
-
-    @ColorInt
-    private final int bulletPointColor;
-
-    @ColorInt
-    private final int codeBackgroundColor;
-
-    @Nullable
-    private final Typeface codeBlockTypeface;
-
-    @NonNull
-    private final Parser parser;
-
-    public MarkdownBuilder(
-            @ColorInt final int bulletPointColor,
-            @ColorInt final int codeBackgroundColor,
-            final Typeface codeBlockTypeface,
-            @NonNull final Parser parser
-    ) {
-        this.bulletPointColor = bulletPointColor;
-        this.codeBackgroundColor = codeBackgroundColor;
-        this.codeBlockTypeface = codeBlockTypeface;
-        this.parser = parser;
-    }
-
-    public SpannedString markdownToSpans(@NonNull final String string) {
-        TextMarkdown markdown = parser.parse(string);
+class MarkdownBuilder(
+    @field:ColorInt @param:ColorInt private val bulletPointColor: Int,
+    @field:ColorInt @param:ColorInt private val codeBackgroundColor: Int,
+    private val codeBlockTypeface: Typeface?,
+    private val parser: Parser
+) {
+    fun markdownToSpans(string: String): SpannedString {
+        val markdown = parser.parse(string)
 
         // In the SpannableStringBuilder, the text and the markup are mutable.
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-        for (int i = 0; i < markdown.getElements().size(); i++) {
-            buildSpans(markdown.getElements().get(i), builder);
+        val builder = SpannableStringBuilder()
+        for (i in markdown.elements.indices) {
+            buildSpans(markdown.elements[i], builder)
         }
-        return new SpannedString(builder);
+        return SpannedString(builder)
     }
 
     /**
      * Build the spans for an element and insert them in the builder
      *
      * @param element the element for which the spans are built
-     * @param builder a {@link SpannableStringBuilder} that gathers all the spans
+     * @param builder a [SpannableStringBuilder] that gathers all the spans
      */
-    private void buildSpans(@NonNull final Element element,
-                            @NonNull final SpannableStringBuilder builder) {
+    private fun buildSpans(element: Element, builder: SpannableStringBuilder) {
         // apply different spans depending on the type of the element
-        switch (element.getType()) {
-            case CODE_BLOCK:
-                buildCodeBlockSpan(element, builder);
-                break;
-            case QUOTE:
-                buildQuoteSpans(element, builder);
-                break;
-            case BULLET_POINT:
-                buildBulletPointSpans(element, builder);
-                break;
-            case TEXT:
-                builder.append(element.getText());
-                break;
+        when (element.type) {
+            Element.Type.CODE_BLOCK -> buildCodeBlockSpan(element, builder)
+            Element.Type.QUOTE -> buildQuoteSpans(element, builder)
+            Element.Type.BULLET_POINT -> buildBulletPointSpans(element, builder)
+            Element.Type.TEXT -> builder.append(element.text)
         }
     }
 
-    private void buildBulletPointSpans(@NonNull final Element element,
-                                       @NonNull final SpannableStringBuilder builder) {
-        int startIndex = builder.length();
-        for (Element child : element.getElements()) {
-            buildSpans(child, builder);
+    private fun buildBulletPointSpans(element: Element, builder: SpannableStringBuilder) {
+        val startIndex = builder.length
+        for (child in element.elements) {
+            buildSpans(child, builder)
         }
-        builder.setSpan(new BulletPointSpan(20, bulletPointColor), startIndex,
-                builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.setSpan(
+            BulletPointSpan(20, bulletPointColor),
+            startIndex,
+            builder.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 
-    private void buildQuoteSpans(@NonNull Element element, SpannableStringBuilder builder) {
-        int startIndex = builder.length();
-        builder.append(element.getText());
+    private fun buildQuoteSpans(element: Element, builder: SpannableStringBuilder) {
+        val startIndex = builder.length
+        builder.append(element.text)
         // You can set multiple spans for the same text
-        builder.setSpan(new StyleSpan(Typeface.ITALIC), startIndex, builder.length(),
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        builder.setSpan(new LeadingMarginSpan.Standard(40), startIndex, builder.length(),
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        builder.setSpan(new RelativeSizeSpan(1.1f), startIndex, builder.length(),
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.setSpan(
+            StyleSpan(Typeface.ITALIC),
+            startIndex,
+            builder.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        builder.setSpan(
+            LeadingMarginSpan.Standard(40),
+            startIndex,
+            builder.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        builder.setSpan(
+            RelativeSizeSpan(1.1f),
+            startIndex,
+            builder.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 
-    private void buildCodeBlockSpan(@NonNull Element element, SpannableStringBuilder builder) {
-        int startIndex = builder.length();
-        builder.append(element.getText());
-        builder.setSpan(new CodeBlockSpan(codeBlockTypeface, codeBackgroundColor), startIndex,
-                builder.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    private fun buildCodeBlockSpan(element: Element, builder: SpannableStringBuilder) {
+        val startIndex = builder.length
+        builder.append(element.text)
+        builder.setSpan(
+            CodeBlockSpan(codeBlockTypeface!!, codeBackgroundColor),
+            startIndex,
+            builder.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 }
