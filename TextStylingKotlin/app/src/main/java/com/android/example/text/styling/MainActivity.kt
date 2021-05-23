0b275987facb68217abfffc1d1b2003f732284a1
markgray <mark.t.gray@gmail.com>
2021-05-23 05:59:56 -0400

    Added comments to MainActivity in TextStylingKotlin

--- TextStylingKotlin/app/src/main/java/com/android/example/text/styling/MainActivity.kt
+++ TextStylingKotlin/app/src/main/java/com/android/example/text/styling/MainActivity.kt
@@ -18,6 +18,7 @@ package com.android.example.text.styling
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import android.graphics.Typeface
 import com.android.example.text.styling.parser.Parser
 import com.android.example.text.styling.renderer.MarkdownBuilder
 
@@ -27,6 +28,29 @@ import com.android.example.text.styling.renderer.MarkdownBuilder
  */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * then we set our content view to our layout file [R.layout.activity_main] which consists of a
+     * `ConstraintLayout` root view holding a single [TextView]. We initialize our [Int] variable
+     * `val bulletPointColor` by retrieving the color [R.color.colorAccent] which is `FF4081` -- a
+     * pastel shade of red. We initialize our [Int] variable `val codeBackgroundColor` by retrieving
+     * the color [R.color.code_background] which is `BBBBBB` -- a light gray.
+     *
+     * We then initialize our [Typeface] variable `val codeBlockTypeface` by using the `font-family`
+     * which is defined in the font/inconsolata.xml file to download the "Inconsolata" font from the
+     * font provider "com.google.android.gms.fonts".
+     *
+     * We construct a [MarkdownBuilder] using `bulletPointColor` as the color to be used for bullet
+     * point spans, using `codeBackgroundColor` as the color to be used for the background color of
+     * code blocks, using `codeBlockTypeface` as the [Typeface] to be used for code blocks, and a
+     * new instance of [Parser] to be used to parse the markdown text. We then call the method
+     * `markdownToSpans` of that [MarkdownBuilder] to have it create a [CharSequence] from the
+     * markdown [String] with the resource ID [R.string.display_text] and use the `run` extension
+     * function on that [CharSequence] to execute a lambda which finds the [TextView] with ID
+     * [R.id.styledText] and sets the `text` of that [TextView] to `this` [CharSequence].
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
