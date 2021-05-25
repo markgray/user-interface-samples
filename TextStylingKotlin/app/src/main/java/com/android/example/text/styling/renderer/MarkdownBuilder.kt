057288e484a93d92d9a07d0da3f09023d3922b9e
markgray <mark.t.gray@gmail.com>
2021-05-25 07:40:57 -0400

    Added comments to MarkdownBuilder in TextStylingKotlin

--- TextStylingKotlin/app/src/main/java/com/android/example/text/styling/renderer/MarkdownBuilder.kt
+++ TextStylingKotlin/app/src/main/java/com/android/example/text/styling/renderer/MarkdownBuilder.kt
@@ -56,7 +56,7 @@ class MarkdownBuilder(
      * to populate a newly created [SpannableStringBuilder] receiver which it then converts to a
      * [SpannedString]. In our lambda we iterate over all of the [Element]s in the `elements` list
      * of `markdown` calling our [buildElement] method with each element using `this` as the
-     * [SpannableStringBuilder] it adds spans to for each [Element].
+     * [SpannableStringBuilder] that it adds spans to for each [Element].
      *
      * @param string the [String] we are to parse as markdown formatted text and convert into a
      * rich text [SpannedString].
@@ -73,7 +73,39 @@ class MarkdownBuilder(
         }
     }
 
-    private fun buildElement(element: Element, builder: SpannableStringBuilder): CharSequence {
+    /**
+     * Interprets the [Element.Type] of its [Element] parameter [element] and adds a span to its
+     * [SpannableStringBuilder] parameter [builder] whose type depends on the type of the element.
+     * We return the value returned when we call the [SpannableStringBuilder.apply] extension function
+     * of our [SpannableStringBuilder] parameter [builder] when it applies our lambda to that
+     * [SpannableStringBuilder]. Our lambda branches on the `type` property of our [Element] parameter
+     * [element]:
+     *  - [Element.Type.CODE_BLOCK] we call the [SpannableStringBuilder.inSpans] method of `this`
+     *  [SpannableStringBuilder] to add a [CodeBlockSpan] that uses our [codeBlockTypeface] field as
+     *  its [Typeface] and our [codeBackgroundColor] field as its background color then append the
+     *  [Element.text] field of [element] to `this` [SpannableStringBuilder].
+     *  - [Element.Type.QUOTE] we call the [SpannableStringBuilder.inSpans] method of `this`
+     *  [SpannableStringBuilder] to add a [StyleSpan] of type [Typeface.ITALIC], an [LeadingMarginSpan]
+     *  for all lines of 40, and a [RelativeSizeSpan] of 1.1f then append the [Element.text] field of
+     *  [element] to `this` [SpannableStringBuilder].
+     *  - [Element.Type.BULLET_POINT] we call the [SpannableStringBuilder.inSpans] method of `this`
+     *  [SpannableStringBuilder] to add a [BulletPointSpan] with a gap width of 40 that uses our
+     *  field [bulletPointColor] as the color of the bullet point then for all of the `child`
+     *  [Element]s in the [Element.elements] field of [element] we make a recursive call to
+     *  [buildElement] with that `child` and our [SpannableStringBuilder] parameter [builder] to
+     *  add spans for those [Element]s.
+     *  - [Element.Type.TEXT] - we just call the [SpannableStringBuilder.append] method of `this`
+     *  [SpannableStringBuilder] to have it append the [Element.text] field of [element] to itself.
+     *
+     * @param element the [Element] whose text we are to wrap in a span that is appropriate for the
+     * [Element.Type] of the [Element].
+     * @param builder the [SpannableStringBuilder] we are to add our spans to.
+     * @return our [SpannableStringBuilder] parameter [builder] to allow chaining.
+     */
+    private fun buildElement(
+        element: Element,
+        builder: SpannableStringBuilder
+    ): SpannableStringBuilder {
         return builder.apply {
             // apply different spans depending on the type of the element
             when (element.type) {
