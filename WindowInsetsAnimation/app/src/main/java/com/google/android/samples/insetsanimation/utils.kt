7af942b34f0af35ccf9577559a5df58f3cce86a1
markgray <mark.t.gray@gmail.com>
2021-06-14 07:26:09 -0400

    Added comments to utils.kt in WindowInsetsAnimation

--- WindowInsetsAnimation/app/src/main/java/com/google/android/samples/insetsanimation/utils.kt
+++ WindowInsetsAnimation/app/src/main/java/com/google/android/samples/insetsanimation/utils.kt
@@ -22,11 +22,25 @@ import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 
-
+/**
+ * A temporary [IntArray] which we use to retrieve the X Y coordinates of a view in its window.
+ */
 private val tmpIntArr = IntArray(2)
 
 /**
- * Function which updates the given [rect] with this view's position and bounds in its window.
+ * Extension function which updates the given [rect] with this view's position and bounds in its
+ * window. If `this` view has been through at least one layout since it was last attached to or
+ * detached from a window and is currently attached to a window we set our [Rect] parameter [rect]
+ * to have its left top at (0,0) and right bottom at the width and height of the [View]. Then we use
+ * the [View.getLocationInWindow] method of our [View] to store the X,Y location of the [View] in
+ * our temp [IntArray] field [tmpIntArr] and finally use the [Rect.offset] method of [rect] to
+ * offset the [Rect] by adding `tmpIntArr[0]` (X coordinate in window) to its left and right
+ * coordinates, and adding `tmpIntArr[1]` (Y coordinate in window) to its top and bottom coordinates.
+ *
+ * If our [View] has not been laid out, or is not currently attached to a window we throw an
+ * [IllegalArgumentException] explaining why we cannot copy the bounds.
+ *
+ * @param rect a [Rect] in which to store the position and bounds of the receiver [View].
  */
 fun View.copyBoundsInWindow(rect: Rect) {
     if (isLaidOut && isAttachedToWindow) {
@@ -42,7 +56,12 @@ fun View.copyBoundsInWindow(rect: Rect) {
 }
 
 /**
- * Provides access to the hidden ViewGroup#suppressLayout method.
+ * Provides access to the hidden [ViewGroup.suppressLayout] method. If our device is running SDK 29
+ * or greater we just call the [ViewGroup.suppressLayout] method directly, otherwise we have to call
+ * our [hiddenSuppressLayout] method.
+ *
+ * @param suppress if `true` suppress all layout() calls until layout suppression is disabled with a
+ * later call to [suppressLayoutCompat] with `false`.
  */
 fun ViewGroup.suppressLayoutCompat(suppress: Boolean) {
     if (Build.VERSION.SDK_INT >= 29) {
@@ -57,6 +76,10 @@ fun ViewGroup.suppressLayoutCompat(suppress: Boolean) {
  */
 private var tryHiddenSuppressLayout = true
 
+/**
+ * Tries to access the hidden [ViewGroup.suppressLayout] method if our [tryHiddenSuppressLayout] is
+ * `true`, setting [tryHiddenSuppressLayout] to `false` if it fails the first time we try it.
+ */
 @SuppressLint("NewApi") // Lint doesn't know about the hidden method.
 private fun hiddenSuppressLayout(group: ViewGroup, suppress: Boolean) {
     if (tryHiddenSuppressLayout) {
