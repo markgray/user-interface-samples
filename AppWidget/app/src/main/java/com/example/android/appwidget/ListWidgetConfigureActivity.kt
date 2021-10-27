c90224de8efdc099bf10cae42e1b2be3e9e523ab
markgray <mark.t.gray@gmail.com>
2021-10-27 06:25:58 -0400

    Added comments to ListWidgetConfigureActivity.kt in AppWidget

--- AppWidget/app/src/main/java/com/example/android/appwidget/ListWidgetConfigureActivity.kt
+++ AppWidget/app/src/main/java/com/example/android/appwidget/ListWidgetConfigureActivity.kt
@@ -16,20 +16,80 @@
 
 package com.example.android.appwidget
 
+import android.app.Activity
 import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.RemoteViews
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.appwidget.databinding.ActivityWidgetConfigureBinding
 
 /**
- * The configuration screen for the [ListAppWidget] widget.
+ * The configuration screen for the [ListAppWidget] widget. Its has an `<intent-filter>` in the
+ * AndroidManifest.xml whose action name is "android.appwidget.action.APPWIDGET_UPDATE" and is
+ * the value of an `android:configure` attribute of the `<appwidget-provider>` file
+ * xml/app_widget_info_checkbox_list.xml, and this file is referenced in a `<meta-data>` attribute
+ * as the android:name="android.appwidget.provider" for the `<receiver>` [ListAppWidget].
+ * An "android.appwidget.action.APPWIDGET_UPDATE" broadcast Intent is sent when it is time to update
+ * your AppWidget. The android:configure attribute names this Activity as the configuration activity
+ * of [ListAppWidget].
  */
 class ListWidgetConfigureActivity : AppCompatActivity() {
 
+    /**
+     * The `appWidgetId` of the widget we are to configure that the AppWidget manager sent us in the
+     * [AppWidgetManager.EXTRA_APPWIDGET_ID] extra of the [Intent] that launched us. We set it in
+     * our [onCreate] override.  [AppWidgetManager.INVALID_APPWIDGET_ID] is a sentinel value that
+     * the AppWidget manager will never return as a `appWidgetId` so we use it as the value until we
+     * know which app widget we are to configure. The [ListAppWidget.updateAppWidget] method uses
+     * this `appWidgetId` when it calls the [AppWidgetManager.updateAppWidget] method to set the
+     * [RemoteViews] to use for the specified appWidgetId.
+     */
     private var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
 
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`.
+     * We then set our result to [Activity.RESULT_CANCELED] (This will cause the widget host to
+     * cancel out of the widget placement if the user presses the back button). Next we initialize
+     * our [ActivityWidgetConfigureBinding] variable `val binding` to the instance that the
+     * [ActivityWidgetConfigureBinding.inflate] method inflates and binds to when it uses the
+     * [LayoutInflater] instance that this Window retrieved from its Context to inflate its
+     * associated layout file layout/activity_widget_configure.xml (resource ID
+     * [R.layout.activity_widget_configure]). This layout file consists of a vertical `LinearLayout`
+     * root view which holds two vertical `LinearLayout`'s which hold a "Grocery List" and a
+     * "To-do List" which consist of a bunch of `CheckBox` widgets for items on the list. Having
+     * inflated our [ActivityWidgetConfigureBinding] we set our content view to the outermost `View`
+     * in the layout file associated [ActivityWidgetConfigureBinding], and then set the title
+     * associated with this activity to the string "Select list for widgets".
+     *
+     * We next set the [View.OnClickListener] of the [ActivityWidgetConfigureBinding.groceryListContainer]
+     * `LinearLayout` which holds the "Grocery List" to a lambda which calls our [onWidgetContainerClicked]
+     * method with the resource ID [R.layout.widget_grocery_list] which refers to the layout file
+     * layout/widget_grocery_list.xml (which consists of a bunch of `CheckBox` widgets whose
+     * android:text attributes are names of grocery items).
+     *
+     * Then we set the [View.OnClickListener] of the [ActivityWidgetConfigureBinding.todoListContainer]
+     * `LinearLayout` which holds the "To-do List" to a lambda which calls our [onWidgetContainerClicked]
+     * method with the resource ID [R.layout.widget_todo_list] which refers to the layout file
+     * layout/widget_todo_list.xml (which consists of a bunch of `CheckBox` widgets whose
+     * android:text attributes are names of household chores).
+     *
+     * We next initialize our [Int] field [appWidgetId] to the value stored in the [Intent] that
+     * launched us under the key [AppWidgetManager.EXTRA_APPWIDGET_ID], defaulting to the value
+     * [AppWidgetManager.INVALID_APPWIDGET_ID] if there is not such key. If [appWidgetId] is
+     * [AppWidgetManager.INVALID_APPWIDGET_ID] this activity was started with an intent without an
+     * app widget ID so we finish with an error by calling the [finish] method. Otherwise we just
+     * hang around until the user clicks the [ActivityWidgetConfigureBinding.groceryListContainer]
+     * or [ActivityWidgetConfigureBinding.todoListContainer] which will then call our
+     * [onWidgetContainerClicked] method with the appropriate layout resource ID for the
+     * [ListAppWidget] to use in its role as a [AppWidgetProvider].
+     *
+     * @param icicle we do not override [onSaveInstanceState] so do not use.
+     */
     public override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
 
