4ddc3d7f0deaaed33457c70ba62e1a530bab5f61
markgray <mark.t.gray@gmail.com>
2022-07-11 05:03:23 -0400

    Updated kotlin to 1.7.10 and added "exclude all" data-extraction-rules in DownloadableFonts

--- DownloadableFonts/app/src/main/java/com/example/android/downloadablefonts/Constants.kt
+++ DownloadableFonts/app/src/main/java/com/example/android/downloadablefonts/Constants.kt
@@ -23,10 +23,12 @@ internal object Constants {
      * The default value to use for the "&width=" query string.
      */
     const val WIDTH_DEFAULT = 100
+
     /**
      * The maximum value to use for the "&width=" query string.
      */
     const val WIDTH_MAX = 1000
+
     /**
      * The minimum value to use for the "&width=" query string.
      */
@@ -36,10 +38,12 @@ internal object Constants {
      * The default value to use for the "&weight=" query string.
      */
     const val WEIGHT_DEFAULT = 400
+
     /**
      * The maximum value to use for the "&weight=" query string.
      */
     const val WEIGHT_MAX = 1000
+
     /**
      * The minimum value to use for the "&weight=" query string.
      */
@@ -49,10 +53,12 @@ internal object Constants {
      * The default value to use for the "&italic=" query string.
      */
     const val ITALIC_DEFAULT = 0f
+
     /**
      * The maximum value to use for the "&italic=" query string.
      */
     const val ITALIC_MAX = 1f
+
     /**
      * The minimum value to use for the "&italic=" query string.
      */
