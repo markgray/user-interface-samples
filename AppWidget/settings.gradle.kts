303592c6a1781bbcd827938f80783b4a4667e8b7
markgray <mark.t.gray@gmail.com>
2024-07-03 05:39:57 -0400

    Converted build files to kts, updated AGP to 8.5.0, and updated gradle dependencies in AppWidget

--- AppWidget/settings.gradle.kts
+++ AppWidget/settings.gradle.kts
@@ -14,24 +14,19 @@
  * limitations under the License.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    ext {
-        kotlin_version = "1.9.24"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
 }
-
-tasks.register("clean", Delete) {
-    delete (rootProject.layout.buildDir)
-}
+rootProject.name = "AppWidget"
+include(":app")
