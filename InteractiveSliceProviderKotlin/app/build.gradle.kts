12622832f5e7b76782cd08c3b79a0ef4af49531f
markgray <mark.t.gray@gmail.com>
2024-07-04 06:31:29 -0400

    Converted build files to kts, updated AGP to 8.5.0, and updated gradle dependencies in InteractiveSliceProviderKotlin

--- InteractiveSliceProviderKotlin/app/build.gradle.kts
+++ InteractiveSliceProviderKotlin/app/build.gradle.kts
@@ -1,12 +1,15 @@
-apply plugin: "com.android.application"
-apply plugin: "kotlin-android"
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
 
 android {
     compileSdk = 34
     defaultConfig {
         applicationId = "com.example.android.interactivesliceprovider"
-        minSdkVersion (23)
-        targetSdkVersion (34)
+        minSdk = 23
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -20,31 +23,32 @@ android {
     }
     buildTypes {
         release {
-            minifyEnabled (false)
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     namespace = "com.example.android.interactivesliceprovider"
 }
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.7.0")
-    implementation ("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
 
     // The slice builder ktx library has a number of dependencies. For reference, since this is a
     // slice sample, below are a list of the slice dependencies:
     // implementation "androidx.slice:slice-core:latest-version"
     // implementation "androidx.slice:slice-builders:latest-version"
-    implementation ("androidx.slice:slice-core:1.0.0")
-    implementation ("androidx.slice:slice-builders:1.0.0")
+    implementation("androidx.slice:slice-core:1.0.0")
+    implementation("androidx.slice:slice-builders:1.0.0")
     //noinspection GradleDependency TODO: figure out why the newer versions use `val` for header
-    implementation ("androidx.slice:slice-builders-ktx:1.0.0-alpha3")
+    implementation("androidx.slice:slice-builders-ktx:1.0.0-alpha3")
 
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.google.firebase:firebase-appindexing:20.0.0")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test:runner:1.5.2")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-appindexing:20.0.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
-
-apply plugin: "com.google.gms.google-services"
