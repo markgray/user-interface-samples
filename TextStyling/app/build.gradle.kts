d5527822fdc7a2759b8cd966d579a4d0d016d2ef
markgray <mark.t.gray@gmail.com>
2024-07-05 05:40:29 -0400

    Converted build files to kts, updated AGP to 8.5.0, and updated gradle dependencies in TextStyling

--- TextStyling/app/build.gradle.kts
+++ TextStyling/app/build.gradle.kts
@@ -13,27 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-apply plugin: "com.android.application"
-apply plugin: "kotlin-android"
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.android.example.text.styling"
-        minSdkVersion (21)
-        targetSdkVersion (34)
+        minSdk = 21
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
         release {
+            @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
             postprocessing {
-                removeUnusedCode = false
-                removeUnusedResources = false
-                obfuscate = false
-                optimizeCode = false
+                isRemoveUnusedCode = false
+                isRemoveUnusedResources = false
+                isObfuscate = false
+                isOptimizeCode = false
                 proguardFile("proguard-rules.pro")
             }
         }
@@ -49,20 +52,16 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.7.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Dependencies for local unit tests
-    testImplementation ("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation ("org.mockito:mockito-core:4.6.1")
-    androidTestImplementation ("androidx.test:runner:1.5.2")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("com.google.dexmaker:dexmaker:1.2")
-    androidTestImplementation ("com.google.dexmaker:dexmaker-mockito:1.2")
-    implementation ("androidx.core:core-ktx:1.13.1")
-}
-repositories {
-    google()
-    mavenCentral()
+    androidTestImplementation("org.mockito:mockito-core:4.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("com.google.dexmaker:dexmaker:1.2")
+    androidTestImplementation("com.google.dexmaker:dexmaker-mockito:1.2")
+    implementation("androidx.core:core-ktx:1.13.1")
 }
