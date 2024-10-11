0ebb734534801446bc68f071f05bd13e808af46f
markgray <mark.t.gray@gmail.com>
2024-10-11 06:30:08 -0400

    Updated date of gradle-wrapper.properties, and fixed warning in EmojiCompat

--- EmojiCompat/app/src/main/java/com/example/android/emojicompat/CustomTextView.kt
+++ EmojiCompat/app/src/main/java/com/example/android/emojicompat/CustomTextView.kt
@@ -59,7 +59,7 @@ class CustomTextView @JvmOverloads constructor(
      * @param filters the current list of [InputFilter]s used by our [AppCompatTextView] super.
      */
     override fun setFilters(filters: Array<InputFilter>) {
-        super.setFilters(emojiTextViewHelper.getFilters(filters))
+        super.filters = emojiTextViewHelper.getFilters(filters)
     }
 
     /**
