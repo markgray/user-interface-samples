1fece6acbc536f149ada41ef9dd2c4c40647a6d0
Mark Gray <mark.t.gray@gmail.com>
2024-10-24 03:46:06 -0400

    Updated AGP to 8.7.1 and updated gradle dependency in DarkTheme

--- DarkTheme/Application/build.gradle.kts
+++ DarkTheme/Application/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
