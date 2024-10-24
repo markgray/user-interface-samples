a7f449fccc1f577909846773f9fba293b52050fb
Mark Gray <mark.t.gray@gmail.com>
2024-10-24 04:02:24 -0400

    Updated AGP to 8.7.1 and updated gradle dependencies in DragAndDropAcrossApps

--- DragAndDropAcrossApps/DragSource/build.gradle.kts
+++ DragAndDropAcrossApps/DragSource/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
     implementation("androidx.core:core-ktx:1.13.1")
