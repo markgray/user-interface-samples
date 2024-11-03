b64f6314d9d0ff27ec7176447e4af4e5608fd024
markgray <mark.t.gray@gmail.com>
2024-11-03 03:51:32 -0500

    Updated AGP to 8.7.2, updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependencies in AndroidOnChromeOSDragAndDropDemo

--- AndroidOnChromeOSDragAndDropDemo/app/build.gradle.kts
+++ AndroidOnChromeOSDragAndDropDemo/app/build.gradle.kts
@@ -20,11 +20,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.hadrosaur.draganddropdemo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -51,9 +51,9 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
 }
