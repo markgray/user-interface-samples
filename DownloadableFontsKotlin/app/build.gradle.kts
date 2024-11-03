6f2cdc4789d3224168895442550e3b4895d38fac
markgray <mark.t.gray@gmail.com>
2024-11-03 05:00:52 -0500

    Updated AGP to 8.7.2, updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependency in DownloadableFontsKotlin

--- DownloadableFontsKotlin/app/build.gradle.kts
+++ DownloadableFontsKotlin/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.downloadablefonts"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
     }
     buildTypes {
         release {
@@ -26,7 +26,7 @@ android {
         implementation("androidx.cardview:cardview:1.0.0")
         implementation("androidx.appcompat:appcompat:1.7.0")
         implementation("com.google.android.material:material:1.12.0")
-        implementation("androidx.core:core-ktx:1.13.1")
+        implementation("androidx.core:core-ktx:1.15.0")
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
