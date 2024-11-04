886e69a109fd53b017c77281161f0ede4a7df8d6
markgray <mark.t.gray@gmail.com>
2024-11-04 05:54:48 -0500

    Updated AGP to 8.7.2, updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in PreferencesKotlin

--- PreferencesKotlin/app/build.gradle.kts
+++ PreferencesKotlin/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.androidx.preference.sample"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
