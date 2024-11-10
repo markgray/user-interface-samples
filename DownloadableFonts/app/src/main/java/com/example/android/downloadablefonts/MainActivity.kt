9ea50c54a145fd6bc509c095582d89eeb66e14a8
Mark Gray <mark.t.gray@gmail.com>
2024-11-10 03:48:08 -0500

    Updated date of gradle-wrapper.properties in DownloadableFonts

--- DownloadableFonts/app/src/main/java/com/example/android/downloadablefonts/MainActivity.kt
+++ DownloadableFonts/app/src/main/java/com/example/android/downloadablefonts/MainActivity.kt
@@ -408,6 +408,7 @@ class MainActivity : AppCompatActivity() {
                 mRequestDownloadButton.isEnabled = true
             }
         }
+        // TODO: use Executor based loading/callback,
         FontsContractCompat
             .requestFont(this@MainActivity, request, callback, handlerThreadHandler)
     }
