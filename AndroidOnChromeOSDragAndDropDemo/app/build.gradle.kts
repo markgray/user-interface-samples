f6d77dd460e4a6709570dec8f5b453a3f7567282
Mark Gray <mark.t.gray@gmail.com>
2025-01-23 03:28:55 -0500

    Updated gradle dependency in AndroidOnChromeDragAndDropDemo

--- AndroidOnChromeOSDragAndDropDemo/app/build.gradle.kts
+++ AndroidOnChromeOSDragAndDropDemo/app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
