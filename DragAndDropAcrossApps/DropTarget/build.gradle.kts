37b4e6d235da2b489f2b64c91ae7a377ea457e81
markgray <mark.t.gray@gmail.com>
2025-01-23 07:24:49 -0500

    Updated gradle dependencies in DragAndDropAcrossApps

--- DragAndDropAcrossApps/DropTarget/build.gradle.kts
+++ DragAndDropAcrossApps/DropTarget/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
     implementation("androidx.core:core-ktx:1.15.0")
