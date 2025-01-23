14e1f0b97f698d5a5ff5e1827b8bdaa957a55af5
markgray <mark.t.gray@gmail.com>
2025-01-23 06:57:29 -0500

    Updated gradle dependency in DownloadableFontsKotlin

--- DownloadableFontsKotlin/app/build.gradle.kts
+++ DownloadableFontsKotlin/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         }
     }
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.9.3")
+        implementation("androidx.activity:activity-ktx:1.10.0")
         implementation("androidx.legacy:legacy-support-v4:1.0.0")
         implementation("androidx.legacy:legacy-support-v13:1.0.0")
         implementation("androidx.cardview:cardview:1.0.0")
