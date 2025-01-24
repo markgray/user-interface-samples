dc97866c173aab597adb03006256463e4b817ab6
markgray <mark.t.gray@gmail.com>
2025-01-24 07:34:14 -0500

    Updated gradle dependency in TextStyling

--- TextStyling/app/build.gradle.kts
+++ TextStyling/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
