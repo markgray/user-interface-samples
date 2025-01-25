87ff26dd9b3f456b5a70e66edd555816622339ea
Mark Gray <mark.t.gray@gmail.com>
2025-01-25 03:31:28 -0500

    Updated gradle dependency in WindowInsetsAnimation

--- WindowInsetsAnimation/app/build.gradle.kts
+++ WindowInsetsAnimation/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.dynamicanimation:dynamicanimation-ktx:1.0.0-alpha03")
     implementation("androidx.appcompat:appcompat:1.7.0")
