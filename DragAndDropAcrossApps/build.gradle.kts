8751b925e6d0fcb83b8a74f0cb1790e73e2d85cb
Mark Gray <mark.t.gray@gmail.com>
2025-02-20 04:10:11 -0500

    Updated AGP to 8.8.1 in DragAndDropAcrossApps

--- DragAndDropAcrossApps/build.gradle.kts
+++ DragAndDropAcrossApps/build.gradle.kts
@@ -15,6 +15,6 @@
  */
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
+    id("com.android.application") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
 }
