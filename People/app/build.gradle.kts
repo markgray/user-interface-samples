1f8df6c22d1dc2faaff80a7aa4d8bbb7a1172531
markgray <mark.t.gray@gmail.com>
2025-03-05 07:14:37 -0500

    Updated AGP to 8.9.0 and updated gradle dependency in People

--- People/app/build.gradle.kts
+++ People/app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
