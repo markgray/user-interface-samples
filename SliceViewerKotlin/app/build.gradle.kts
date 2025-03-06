e8b2501605e0c87b4179c77d05ae6bdea1915602
Mark Gray <mark.t.gray@gmail.com>
2025-03-06 03:40:24 -0500

    Updated AGP to 8.9.0 and updated gradle dependencies in SliceViewerKotlin

--- SliceViewerKotlin/app/build.gradle.kts
+++ SliceViewerKotlin/app/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.slice:slice-view:1.1.0-alpha02")
     implementation("androidx.slice:slice-core:1.0.0")
     implementation("androidx.slice:slice-builders:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
