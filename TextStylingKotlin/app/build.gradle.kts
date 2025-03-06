a90c94c064e11c510e5fa5c60e48375e082855b6
Mark Gray <mark.t.gray@gmail.com>
2025-03-06 04:50:38 -0500

    Updated AGP to 8.9.0 and updated gradle dependencies in TextStylingKotlin

--- TextStylingKotlin/app/build.gradle.kts
+++ TextStylingKotlin/app/build.gradle.kts
@@ -52,9 +52,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Dependencies for local unit tests
     testImplementation("junit:junit:4.13.2")
