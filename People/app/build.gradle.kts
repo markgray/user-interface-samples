cc00498b8c512e2b9952bdc5984d9230cf4940d8
markgray <mark.t.gray@gmail.com>
2025-04-12 07:25:28 -0400

    Updated gradle dependency in People

--- People/app/build.gradle.kts
+++ People/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.12.0")
