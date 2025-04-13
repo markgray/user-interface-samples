dfc35546b7e5bc72f4ba6355f8d24c708e42bb83
Mark Gray <mark.t.gray@gmail.com>
2025-04-13 03:27:49 -0400

    Updated gradle dependency in WindowManager

--- WindowManager/app/build.gradle.kts
+++ WindowManager/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.window:window:1.3.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
