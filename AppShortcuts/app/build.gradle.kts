a3b391b29a069a6248d38bd259410a73ecd35a27
Mark Gray <mark.t.gray@gmail.com>
2025-04-30 03:33:56 -0400

    Updated AGP to 8.9.2 and updated gradle dependencies in AppShortcuts

--- AppShortcuts/app/build.gradle.kts
+++ AppShortcuts/app/build.gradle.kts
@@ -33,6 +33,6 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 }
