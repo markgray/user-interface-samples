d6620f61bf6deeeaf641391bb58509e1a811b061
markgray <mark.t.gray@gmail.com>
2025-05-11 05:39:21 -0400

    Updated AGP to 8.10.0 and updated gradle to 8.14 in EmojiCompat

--- EmojiCompat/build.gradle.kts
+++ EmojiCompat/build.gradle.kts
@@ -15,6 +15,6 @@
  */
 
 plugins {
-    id("com.android.application") version "8.9.2" apply false
+    id("com.android.application") version "8.10.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
 }
