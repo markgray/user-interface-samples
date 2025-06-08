b817012f102c91bb22c6967129ff98253cd71d47
Mark Gray <mark.t.gray@gmail.com>
2025-06-08 04:07:40 -0400

    Updated AGP to 8.10.1, and updated gradle to 8.14.1 and updated gradle dependency in TextStyling

--- TextStyling/app/build.gradle.kts
+++ TextStyling/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Dependencies for local unit tests
