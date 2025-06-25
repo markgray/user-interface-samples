d4acc8c0d66edeae66a9001bc655c3a9563e32d9
markgray <mark.t.gray@gmail.com>
2025-06-25 07:12:03 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated gradle to 8.14.2, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" in WindowManagerSample

--- WindowManager/app/build.gradle.kts
+++ WindowManager/app/build.gradle.kts
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.windowmanagersample"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -33,8 +35,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     buildFeatures {
         viewBinding = true
