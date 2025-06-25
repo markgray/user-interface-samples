fb435bf4bf0bedd21e005ba280e2eeaf6c9f86f2
Mark Gray <mark.t.gray@gmail.com>
2025-06-25 03:33:42 -0400

    Update targetSdk to 36, updated AGP to 8.11.0, updated gradle to 8.14.2, updated kotlin to 2.2.0, and update gradle dependency in People

--- People/app/build.gradle.kts
+++ People/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.people"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -50,7 +50,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    testImplementation("org.robolectric:robolectric:4.14.1")
+    testImplementation("org.robolectric:robolectric:4.15.1")
     testImplementation("androidx.test.ext:junit:1.2.1")
     testImplementation("androidx.test.espresso:espresso-core:3.6.1")
     testImplementation("androidx.test.ext:truth:1.6.0")
