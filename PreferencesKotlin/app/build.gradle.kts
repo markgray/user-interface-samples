f72d501515db9232bdb3d7b2f2211e555210ceaa
markgray <mark.t.gray@gmail.com>
2025-07-09 05:42:19 -0400

    Switched to "kotlin compilerOptions" and updated gradle dependencies in PreferencesKotlin

--- PreferencesKotlin/app/build.gradle.kts
+++ PreferencesKotlin/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -21,8 +23,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.androidx.preference.sample"
 }
