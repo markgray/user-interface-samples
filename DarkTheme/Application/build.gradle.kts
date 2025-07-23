974aab3464b93284df033b4100bca3114621f47a
markgray <mark.t.gray@gmail.com>
2025-07-23 06:32:00 -0400

    Updated AGP to 8.11.1, updated gradle to 8.14.3, and updated gradle dependencies in DarkTheme

--- DarkTheme/Application/build.gradle.kts
+++ DarkTheme/Application/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
 }
 
 android {
