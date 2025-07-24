70025192f7f8b155d53e766db8a6bb109ed896b0
Mark Gray <mark.t.gray@gmail.com>
2025-07-24 03:58:57 -0400

    Updated AGP to 8.11.1, updated gradle to 8.14.3, and updated gradle dependencies in People

--- People/app/build.gradle.kts
+++ People/app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
 
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("junit:junit:4.13.2")
