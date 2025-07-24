d5fee3e2bcaae772786eb7933fbe4bad62633071
markgray <mark.t.gray@gmail.com>
2025-07-24 06:42:58 -0400

    Updated AGP to 8.11.1, and updated gradle dependencies in WindowManagerSample

--- WindowManager/app/build.gradle.kts
+++ WindowManager/app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.window:window:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
 
     androidTestImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
