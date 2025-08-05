2db7ae1f4dcecaa9fe8a02231fd80c9a37657056
Mark Gray <mark.t.gray@gmail.com>
2025-08-05 03:40:31 -0400

    Updated AGP to 8.12.0, updated gradle to 9.0.0, and updated gradle dependencies in EmojiCompat

--- EmojiCompat/app/build.gradle.kts
+++ EmojiCompat/app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 
     // Test
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test:rules:1.7.0")
+    androidTestImplementation("androidx.test:runner:1.7.0")
 }
