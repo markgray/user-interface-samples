6f9a5f5d7bd37401478b3fdec7d9c9891b2dd904
markgray <mark.t.gray@gmail.com>
2025-08-16 07:21:44 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in DarkTheme

--- DarkTheme/Application/build.gradle.kts
+++ DarkTheme/Application/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
 }
 
