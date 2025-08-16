98e626b68c1eca72aa75b9703c26962bac5b5d02
markgray <mark.t.gray@gmail.com>
2025-08-16 06:28:12 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in AndroidOnChromeOSDragAndDropDemo

--- AndroidOnChromeOSDragAndDropDemo/app/build.gradle.kts
+++ AndroidOnChromeOSDragAndDropDemo/app/build.gradle.kts
@@ -43,5 +43,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
