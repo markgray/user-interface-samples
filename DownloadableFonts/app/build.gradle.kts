51851d53f06c1865b10989740e1e21557a8fe16d
Mark Gray <mark.t.gray@gmail.com>
2025-08-17 03:09:08 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in DownloadableFonts

--- DownloadableFonts/app/build.gradle.kts
+++ DownloadableFonts/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         implementation("androidx.legacy:legacy-support-v13:1.0.0")
         implementation("androidx.cardview:cardview:1.0.0")
         implementation("androidx.appcompat:appcompat:1.7.1")
-        implementation("androidx.core:core-ktx:1.16.0")
+        implementation("androidx.core:core-ktx:1.17.0")
         implementation("com.google.android.material:material:1.12.0")
     }
     compileOptions {
