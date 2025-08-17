ab2780fa437ad0e757af7e5032975692047559fa
Mark Gray <mark.t.gray@gmail.com>
2025-08-17 04:03:53 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in EmojiCompatKotlin

--- EmojiCompatKotlin/app/src/main/java/com/example/android/emojicompat/CustomTextView.kt
+++ EmojiCompatKotlin/app/src/main/java/com/example/android/emojicompat/CustomTextView.kt
@@ -83,7 +83,7 @@ class CustomTextView @JvmOverloads constructor(
      * @param filters the current list of [InputFilter]s used by our [AppCompatTextView] super.
      */
     override fun setFilters(filters: Array<InputFilter>) {
-        @Suppress("UsePropertyAccessSyntax") // TODO: Property access syntax breaks compilation
+        @Suppress("UsePropertyAccessSyntax", "RedundantSuppression") // TODO: Property access syntax breaks compilation
         super.setFilters(emojiTextViewHelper.getFilters(filters))
     }
 
