ce7b41f430f11146ff9a2cef00bdd58839d7602f
Mark Gray <mark.t.gray@gmail.com>
2025-09-04 03:39:17 -0400

    Updated AGP to 8.13.0, fixed warnings, and updated gradle dependencies in SliceViewerKotlin

--- SliceViewerKotlin/app/build.gradle.kts
+++ SliceViewerKotlin/app/build.gradle.kts
@@ -15,9 +15,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-common:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-common:2.9.3")
     //noinspection GradleDependency TODO: figure out why the newer versions use `val` for header
     implementation("androidx.slice:slice-builders-ktx:1.0.0-alpha3")
 }
