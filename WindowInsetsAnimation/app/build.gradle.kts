037177e1c180d73f4c54315bbae88dc60872c1cd
Mark Gray <mark.t.gray@gmail.com>
2025-09-04 03:56:33 -0400

    Updated AGP to 8.13.0, fixed warnings, and updated gradle dependencies in WindowInsetsAnimation

--- WindowInsetsAnimation/app/build.gradle.kts
+++ WindowInsetsAnimation/app/build.gradle.kts
@@ -35,5 +35,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.dynamicanimation:dynamicanimation-ktx:1.1.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
 }
