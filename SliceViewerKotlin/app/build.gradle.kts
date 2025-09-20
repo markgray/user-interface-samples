4b5dc3c9b5201dffdf52559360b7deecb172ddcf
markgray <mark.t.gray@gmail.com>
2025-09-20 07:01:28 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in SliceViewerKotlin

--- SliceViewerKotlin/app/build.gradle.kts
+++ SliceViewerKotlin/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.slice:slice-view:1.1.0-alpha02")
     implementation("androidx.slice:slice-core:1.0.0")
     implementation("androidx.slice:slice-builders:1.0.0")
@@ -15,9 +15,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-common:2.9.3")
+    implementation("androidx.lifecycle:lifecycle-common:2.9.4")
     //noinspection GradleDependency TODO: figure out why the newer versions use `val` for header
     implementation("androidx.slice:slice-builders-ktx:1.0.0-alpha3")
 }
