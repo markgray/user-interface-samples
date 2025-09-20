1219fa35f57311d13ac79a6caadb2461d501797c
markgray <mark.t.gray@gmail.com>
2025-09-20 06:56:35 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in PreferencesKotlin

--- PreferencesKotlin/app/build.gradle.kts
+++ PreferencesKotlin/app/build.gradle.kts
@@ -32,6 +32,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
 }
