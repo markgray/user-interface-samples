2fcadc9791f7903acdaf34b603561d201ce12122
markgray <mark.t.gray@gmail.com>
2025-10-16 07:38:04 -0400

    Updated date of gradle-wrapper.properties, and added signingConfig for release variant in AppShortcuts

--- AppShortcuts/app/build.gradle.kts
+++ AppShortcuts/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     namespace = "com.example.android.appshortcuts"
