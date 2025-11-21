c6a87f92975258e190efd26f50d4793530a5f87d
Mark Gray <mark.t.gray@gmail.com>
2025-11-21 03:38:45 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies and increased minSdk to 23 in ImmersiveMode

--- ImmersiveMode/app/build.gradle.kts
+++ ImmersiveMode/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.immersivemode"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -28,7 +28,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
 }
