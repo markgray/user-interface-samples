93ef50a4fea84774dddc02fbaf8c1a3aad2cb8d1
markgray <mark.t.gray@gmail.com>
2025-11-21 06:59:15 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies and increased minSdk to 23 in TextStylingKotlin

--- TextStylingKotlin/app/build.gradle.kts
+++ TextStylingKotlin/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.android.example.text.styling"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
