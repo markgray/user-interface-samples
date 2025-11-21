8f511a3bcb1e5c38e72a02e8da7feb2e7af88ba4
markgray <mark.t.gray@gmail.com>
2025-11-21 07:31:38 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies and increased minSdk to 23 in WindowInsetsAnimation

--- WindowInsetsAnimation/app/build.gradle.kts
+++ WindowInsetsAnimation/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.google.android.samples.insetschat"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -31,7 +31,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.dynamicanimation:dynamicanimation-ktx:1.1.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
