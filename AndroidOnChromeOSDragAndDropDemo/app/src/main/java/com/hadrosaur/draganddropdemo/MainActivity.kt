84d478be0418e5777211875e8d06110aee4e5ebc
markgray <mark.t.gray@gmail.com>
2025-11-23 07:37:28 -0500

    Added comments and removed obsolete TODO in AndroidOnChromeOSDragAndDropDemo

--- AndroidOnChromeOSDragAndDropDemo/app/src/main/java/com/hadrosaur/draganddropdemo/MainActivity.kt
+++ AndroidOnChromeOSDragAndDropDemo/app/src/main/java/com/hadrosaur/draganddropdemo/MainActivity.kt
@@ -14,7 +14,6 @@
 */
 package com.hadrosaur.draganddropdemo
 
-import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipDescription
 import android.graphics.Color
@@ -34,6 +33,8 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.Insets
+import androidx.core.view.OnApplyWindowInsetsListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -47,12 +48,26 @@ import java.io.FileNotFoundException
  */
 open class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`. We initialize our
-     * [TextView] variable `val dragText` by finding the view with ID `R.id.text_drag` and our
-     * [FrameLayout] variable `val targetFrame` by finding the view with ID `R.id.frame_target`. We
-     * set the [OnDragListener] of `targetFrame` to an instance of our [DropTargetListener] class,
-     * and set the [OnLongClickListener] of `dragText` to an instance of [TextViewLongClickListener].
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * display then we call our super's implementation of `onCreate`, and set our content view
+     * to our layout file `R.layout.activity_main`. We initialize our [ConstraintLayout] variable
+     * `val rootView` by finding the view with ID `R.id.root_view` and call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to set an [OnApplyWindowInsetsListener] on
+     * `rootView` to take over the policy for applying window insets to `rootView` with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda in variable `v` and
+     * the [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It initializes its
+     * [Insets] variable `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
+     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the
+     * right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so
+     * that the window insets will not be passed down to descendant views).
+     *
+     * Next initialize our [TextView] variable `val dragText` by finding the view with ID
+     * `R.id.text_drag` and our [FrameLayout] variable `val targetFrame` by finding the view with ID
+     * `R.id.frame_target`. We set the [OnDragListener] of `targetFrame` to an instance of our
+     * [DropTargetListener] class, and set the [OnLongClickListener] of `dragText` to an instance of
+     * [TextViewLongClickListener].
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -61,8 +76,8 @@ open class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<ConstraintLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -197,7 +212,6 @@ open class MainActivity : AppCompatActivity() {
                             //Note the use of ContentResolver to resolve the ChromeOS content URI.
                             val contentUri = item.uri
 
-                            @SuppressLint("Recycle") // TODO: Recycle ParcelFileDescriptor
                             val parcelFileDescriptor: ParcelFileDescriptor? = try {
                                 contentResolver.openFileDescriptor(contentUri, "r")
                             } catch (e: FileNotFoundException) {
@@ -227,7 +241,7 @@ open class MainActivity : AppCompatActivity() {
                             params.gravity = Gravity.CENTER
                             droppedText.layoutParams = params
                             droppedText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f)
-                            droppedText.text = contents.substring(0, contentLength)
+                            droppedText.text = contents.take(n = contentLength)
                             frameTarget.addView(droppedText)
                         }
 
