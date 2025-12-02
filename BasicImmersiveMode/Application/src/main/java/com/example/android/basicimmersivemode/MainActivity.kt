9e644b3d081839221164cf69b81783d89aff650c
markgray <mark.t.gray@gmail.com>
2025-12-02 00:33:07 -0500

    Added comments in BasicImmersiveMode

--- BasicImmersiveMode/Application/src/main/java/com/example/android/basicimmersivemode/MainActivity.kt
+++ BasicImmersiveMode/Application/src/main/java/com/example/android/basicimmersivemode/MainActivity.kt
@@ -19,9 +19,11 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -50,11 +52,27 @@ import com.example.android.common.logger.MessageOnlyLogFilter
  */
 class MainActivity : SampleActivityBase() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * and then we set our content view to our layout file `R.layout.activity_main`.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_main`.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID
+     * `R.id.sample_main_layout` then call [ViewCompat.setOnApplyWindowInsetsListener]
+     * to take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda in
+     * variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * If the [FragmentManager] for interacting with fragments associated with this activity is not
-     * able to find a [Fragment] whose tag name is [FRAGTAG] we are being started for the first
+     * able to find a [Fragment] whose tag name is [FRAGTAG], we are being started for the first
      * time so we use the [FragmentManager] for interacting with fragments associated with this
      * activity to begin a [FragmentTransaction] which we save in our variable `val transaction`,
      * then we construct a new instance of [BasicImmersiveModeFragment] to initialize our variable
@@ -68,8 +86,8 @@ class MainActivity : SampleActivityBase() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
