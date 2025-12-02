cde962952e21542a525ee17acd72c41dc00e3f14
markgray <mark.t.gray@gmail.com>
2025-12-02 04:16:12 -0500

    Added comments in EmojiCompatKotlin

--- EmojiCompatKotlin/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
+++ EmojiCompatKotlin/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
@@ -18,12 +18,15 @@ package com.example.android.emojicompat
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.ScrollView
 import androidx.emoji.text.EmojiCompat
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -59,11 +62,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`.
-     * Then we set our content view to our layout file `R.layout.activity_main` which consists of a
-     * `ScrollView` root view holding a vertical `LinearLayout` with an [EmojiAppCompatTextView], an
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_main` which consists of a [ScrollView] root view
+     * holding a vertical [LinearLayout] with an [EmojiAppCompatTextView], an
      * [EmojiAppCompatEditText], an [EmojiAppCompatButton], a [TextView] and our [CustomTextView]
      * custom [TextView].
+     *
+     * We initialize our [ScrollView] variable `rootView`
+     * to the view with ID `R.id.scroll` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * We then proceed to locate each of the above text displaying views in order to set their text
      * to a [CharSequence] which include emojis from [EmojiCompat]:
@@ -84,16 +104,16 @@ class MainActivity : AppCompatActivity() {
      *  [EMOJI] string constant.
      *  - `val regularTextView` is initialized by finding the [TextView] in our UI with resource ID
      *  `R.id.regular_text_view`. This is a regular [TextView] without [EmojiCompat] support so we
-     *  have to manually process the text and to do this we fetch the singleton [EmojiCompat] instance
-     *  and register an instance of our initialization callback [InitCallback] constructed to use the
-     *  singleton [EmojiCompat] to process the text and set the text of `regularTextView` to it in
-     *  its `onInitialized` override (**Note:** if initialization has already occurred `onInitialized`
-     *  is called immediately).
+     *  have to manually process the text and to do this we fetch the singleton [EmojiCompat]
+     *  instance and register an instance of our initialization callback [InitCallback] constructed
+     *  to use the singleton [EmojiCompat] to process the text and set the text of `regularTextView`
+     *  to it in its `onInitialized` override (**Note:** if initialization has already occurred
+     *  `onInitialized` is called immediately).
      *  - `val customTextView` is initialized by finding the [TextView] in our UI with resource ID
      *  `R.id.emoji_custom_text_view`. It is an instance of our [CustomTextView] so we can just set
-     *  its `text` property  to the emoji containing formatted string "Custom TextView [EMOJI]" created
-     *  by calling the [getString] method with the resource ID `R.string.custom_text_view` formatting
-     *  string and the [EMOJI] string constant.
+     *  its `text` property  to the emoji containing formatted string "Custom TextView [EMOJI]"
+     *  created by calling the [getString] method with the resource ID `R.string.custom_text_view`
+     *  formatting string and the [EMOJI] string constant.
      *
      * @param savedInstanceState We do not override [onSaveInstanceState] so do not use.
      */
@@ -102,8 +122,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<ScrollView>(R.id.scroll)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
