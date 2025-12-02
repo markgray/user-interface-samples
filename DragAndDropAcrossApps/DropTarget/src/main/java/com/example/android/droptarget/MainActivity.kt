55bc55c9c86a5c6bf9e3c19b103e365a0bfc3c0e
markgray <mark.t.gray@gmail.com>
2025-12-02 03:44:54 -0500

    Added comments in DragAndDropAcrossApps

--- DragAndDropAcrossApps/DropTarget/src/main/java/com/example/android/droptarget/MainActivity.kt
+++ DragAndDropAcrossApps/DropTarget/src/main/java/com/example/android/droptarget/MainActivity.kt
@@ -24,6 +24,7 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import android.widget.ViewAnimator
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -56,14 +57,31 @@ class MainActivity : SampleActivityBase() {
     private var mLogShown = false
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`. There are two of
-     * these: layout/activity_main.xml used by displays narrower than 720dp, and displays 720dp or
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_main`. There are two of these:
+     * layout/activity_main.xml used by displays narrower than 720dp, and displays 720dp or
      * greater will use layout-w720dp/activity_main.xml with the only difference being that the
      * layout/activity_main.xml file contains a [ViewAnimator] that holds the sample description
      * and a `fragment` used for [LogFragment] with which child is displayed being toggled by our
      * option menu, and layout-w720dp/activity_main.xml displays both the sample description and a
      * `fragment` used for [LogFragment] at the same time.
+     *
+     * We initialize our [LinearLayout] variable `rootView`
+     * to the view with ID `R.id.sample_main_layout` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * If our [Bundle] parameter [savedInstanceState] is `null` it means we are being started for
      * the first time so we use the [FragmentManager] for interacting with fragments associated with
@@ -82,9 +100,8 @@ class MainActivity : SampleActivityBase() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
-        // TODO: Modify marginTop of scrollview here instead of in xml
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
