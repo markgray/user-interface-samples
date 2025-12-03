1ac98852189b6deac6392493f8e5c0e5435ebb74
markgray <mark.t.gray@gmail.com>
2025-12-03 07:07:35 -0500

    Added comments in TextStylingKotlin

--- TextStylingKotlin/app/src/main/java/com/android/example/text/styling/MainActivity.kt
+++ TextStylingKotlin/app/src/main/java/com/android/example/text/styling/MainActivity.kt
@@ -20,8 +20,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.graphics.Typeface
+import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -35,12 +37,31 @@ import com.android.example.text.styling.renderer.MarkdownBuilder
 class MainActivity : AppCompatActivity() {
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main` which consists of a
-     * `ConstraintLayout` root view holding a single [TextView]. We initialize our [Int] variable
-     * `val bulletPointColor` by retrieving the color `R.color.colorAccent` which is `FF4081` -- a
-     * pastel shade of red. We initialize our [Int] variable `val codeBackgroundColor` by retrieving
-     * the color `R.color.code_background` which is `BBBBBB` -- a light gray.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_main` which consists of a [ConstraintLayout]
+     * root view holding a single [TextView].
+     *
+     * We initialize our [ConstraintLayout] variable `rootView`
+     * to the view with ID `R.id.root_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [Int] variable `val bulletPointColor` by retrieving the color
+     * `R.color.colorAccent` which is `FF4081` -- a pastel shade of red. We initialize our [Int]
+     * variable `val codeBackgroundColor` by retrieving the color `R.color.code_background` which
+     * is `BBBBBB` -- a light gray.
      *
      * We then initialize our [Typeface] variable `val codeBlockTypeface` by using the `font-family`
      * which is defined in the font/inconsolata.xml file to download the "Inconsolata" font from the
@@ -62,8 +83,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<ConstraintLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
