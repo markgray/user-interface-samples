ebdeb22997fb11ef21f296e581cfb213260d29e3
markgray <mark.t.gray@gmail.com>
2025-12-03 07:13:04 -0500

    Added comments in WindowInsetsAnimation

--- WindowInsetsAnimation/app/src/main/java/com/google/android/samples/insetsanimation/MainActivity.kt
+++ WindowInsetsAnimation/app/src/main/java/com/google/android/samples/insetsanimation/MainActivity.kt
@@ -24,6 +24,7 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -41,11 +42,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we initialize our [ActivityMainBinding] field [binding] to the value returned when the
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and initialize our
+     * [ActivityMainBinding] field [binding] to the value returned when the
      * [ActivityMainBinding.inflate] uses the [LayoutInflater] instance that this Window retrieved
      * from its [Context] to inflate its associated layout file layout/activity_main.xml, and we
      * set our content view to the outermost [View] in the layout file associated with [binding].
+     *
+     * We call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to the root [View] of [binding], with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
      * Finally we call the [WindowCompat.setDecorFitsSystemWindows] method with the current [Window]
      * of the activity and `false` for its `decorFitsSystemWindows` flag to tell the Window that our
      * app is going to be responsible for fitting any system windows.
@@ -57,8 +74,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -70,7 +88,6 @@ class MainActivity : AppCompatActivity() {
             // down to descendant views.
             WindowInsetsCompat.CONSUMED
         }
-        setContentView(binding.root)
 
         // Tell the Window that our app is going to responsible for fitting for any system windows.
         // This is similar to the now deprecated:
