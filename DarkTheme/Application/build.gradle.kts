59ce4e69fc070cefa3fef0d8c8a3ff9626274a9b
markgray <mark.t.gray@gmail.com>
2025-12-08 23:34:39 -0500

    Updated gradle dependencies in DarkTheme

--- DarkTheme/Application/build.gradle.kts
+++ DarkTheme/Application/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.12.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
