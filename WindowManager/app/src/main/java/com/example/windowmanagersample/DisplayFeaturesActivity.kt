9447a47cd8da3d5538cfbf2203d3a18eeb7b1c25
markgray <mark.t.gray@gmail.com>
2025-12-09 06:40:20 -0500

    Added ime padding in WindowManager

--- WindowManager/app/src/main/java/com/example/windowmanagersample/DisplayFeaturesActivity.kt
+++ WindowManager/app/src/main/java/com/example/windowmanagersample/DisplayFeaturesActivity.kt
@@ -21,8 +21,14 @@ import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -48,16 +54,34 @@ class DisplayFeaturesActivity : AppCompatActivity() {
     private lateinit var windowInfoRepo: WindowInfoTracker
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we initialize our [Activity] variable `val activity` to `this`. We initialize our
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and initialize our
+     * [Activity] variable `val activity` to `this`. We initialize our
      * [ActivityDisplayFeaturesBinding] field [binding] by having the method
      * [ActivityDisplayFeaturesBinding.inflate] use the [LayoutInflater] instance that this Window
      * retrieved from its [Context] to inflate the activity_display_features.xml layout file
      * associated with it to produce an [ActivityDisplayFeaturesBinding] instance, and we set our
-     * content view to the outermost View in the layout file associated with [binding]. We next
-     * initialize our [WindowInfoTracker] field [windowInfoRepo] to an instance of [WindowInfoTracker]
-     * that is associated with our [Context]. Next we launch a coroutine on the `lifecycleScope`
-     * [CoroutineScope] tied to this LifecycleOwner's Lifecycle. We call the
+     * content view to the outermost View in the layout file associated with [binding].
+     *
+     * We call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to the root [View] of [binding], with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using [WindowInsetsCompat.Type.ime]. It then updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We next initialize our [WindowInfoTracker] field [windowInfoRepo] to an instance of
+     * [WindowInfoTracker] that is associated with our [Context]. Next we launch a coroutine
+     * on the `lifecycleScope` [CoroutineScope] tied to this LifecycleOwner's Lifecycle. We call the
      * [Lifecycle.repeatOnLifecycle] method of our [Lifecycle] to have it execute its lambda block
      * when the lifecycle is at least STARTED (it is cancelled when the lifecycle is STOPPED, and
      * automatically restarted when the lifecycle is STARTED again). In that lambda block we call the
@@ -74,11 +98,27 @@ class DisplayFeaturesActivity : AppCompatActivity() {
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         val activity: Activity = this
 
         binding = ActivityDisplayFeaturesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v: View, windowInsets: WindowInsetsCompat ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
 
         windowInfoRepo = WindowInfoTracker.getOrCreate(this)
 
